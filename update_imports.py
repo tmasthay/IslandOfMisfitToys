dda4feba0c4d8e2347d9ab4655f89ede658588c2
tmasthay <tyler@ices.utexas.edu>
2023-04-01 01:19:06 -0700

    simplify update_imports

--- update_imports.py
+++ update_imports.py
@@ -1,9 +1,6 @@
 from import_env import *
 import os
 
-u = get_subfolders(os.getcwd())
-u = [e for e in u if e != 'examples']
-for e in u:
-    init_modules(e)
+init_modules(os.getcwd(), root=True, omissions=['examples'])
 
 
