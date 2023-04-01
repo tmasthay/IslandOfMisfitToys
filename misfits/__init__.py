598e664a1cfcaf12257e7564093a33e8b018f8ff
tmasthay <tyler@ices.utexas.edu>
2023-04-01 01:26:17 -0700

    Update init

--- misfits/__init__.py
+++ misfits/__init__.py
@@ -1,7 +1,15 @@
 __all__ = [
-    "func1",
-    "huber",
     "rom",
-    "ot"
+    "graph_ot",
+    "cross_correlation",
+    "w1",
+    "fisher",
+    "radon_ot",
+    "sambridge_ot",
+    "l1_l2_hybrid",
+    "huber",
+    "sobolev",
+    "w2",
+    "sinkorn_ot"
 ]
 from . import *
