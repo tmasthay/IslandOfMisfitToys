719849e60575fa0a19565240ffb8bf7ae2dc751c
tmasthay <tyler@ices.utexas.edu>
2023-04-05 16:22:51 -0500

    Add quantile make file

---
+++ misfits/helpers/quantile/setup_smart_quantile.py
@@ -0,0 +1,10 @@
+# setup22.py
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy as np
+
+setup(
+    ext_modules = cythonize("smart_quantile.pyx"),
+    include_dirs=[np.get_include()]
+)
+
--- update_imports.py
+++ update_imports.py
@@ -1,6 +1,10 @@
 from Experiments.helpers.import_env import *
 import os
 
-init_modules(os.getcwd(), root=True, omissions=['examples', 'Experiments'])
+init_modules(
+    os.getcwd(), 
+    root=True, 
+    omissions=['examples', 'Experiments']
+)
 
 
