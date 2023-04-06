6e8702f21e0e8b7953e14de4bec33eb0eba69539
tmasthay <tyler@ices.utexas.edu>
2023-04-06 14:06:06 -0500

    Restore original quantile

--- misfits/helpers/quantile/setup_smart_quantile.py
+++ misfits/helpers/quantile/setup_smart_quantile.py
@@ -1,14 +1,10 @@
-# setup_smart_quantile.py
+# setup22.py
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy as np
-import os
-
-extensions = cythonize('smart_quantile.pyx')
 
 setup(
-    name='smart_quantile',
-    ext_modules=extensions,
+    ext_modules = cythonize("smart_quantile_cython.pyx"),
     include_dirs=[np.get_include()]
 )
 
