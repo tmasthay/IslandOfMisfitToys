fdff5854abeb6a36264c771e5363a5c69709e6c8
shenyiran91 <shenyiran91@utexas.edu>
2023-04-10 01:14:49 -0500

    w1.py

--- misfits/w1.py
+++ misfits/w1.py
@@ -1,31 +1,15 @@
 import numpy as np
-from numpy import linalg as LA
 #def func1():
 #    print('func1')
 
 
-def W1_metric(f, g, t, Nt, dt):
-# normalization
-    f = f / (LA.norm(f,1)*dt)
-    g = g / (LA.norm(g,1)*dt)
-# numerical integration
+def W1_metric(f, g, Nt): 
+#CDF
     F = np.zeros(Nt)
     G = np.zeros(Nt)
-    F[0] = f[0]
-    G[0] = g[0]
-    for i in range(1, Nt):
-        F[i] = F[i-1] + f[i]
-        G[i] = G[i-1] + g[i]
-    F = F * dt
-    G = G * dt
-# inverse of G 
-    G_inv = np.zeros(Nt)
     for i in range(0, Nt):
-        y = F[i]
-        ind_g = np.where( y >= G)
-        if size(ind_g) == 0:
-            G_inv[i] = t[end]
-        else:
-            G_inv[i] = t[ind_g[0]]
-    w1 = sum(abs(t-G_inv) * f * dt)
+        F[i] = np.sum(f[0:i])
+        G[i] = np.sum(g[0:i])
+# inverse
+    w1 = np.sum(np.abs(F-G))
     return w1
