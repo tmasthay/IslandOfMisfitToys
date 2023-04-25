980d097f903e94633fed762e6887c5c47c407871
tmasthay <tyler@ices.utexas.edu>
2023-04-25 16:19:49 -0700

    Add setup_smart_quantile back
    Accidentally deleted this during merge

---
+++ misfits/helpers/quantile/setup_smart_quantile.py
@@ -0,0 +1,12 @@
+# setup22.py
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy as np
+
+#irrelevant comment
+
+setup(
+    ext_modules = cythonize("smart_quantile_cython.pyx"),
+    include_dirs=[np.get_include()]
+)
+
