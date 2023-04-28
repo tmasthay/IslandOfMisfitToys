44dee3d246aa3a789dce6813ca8c165367872d2c
shenyiran91 <shenyiran91@utexas.edu>
2023-04-28 00:29:43 -0500

    modified huber & hybrid norm

--- misfits/l1_l2_hybrid.py
+++ misfits/l1_l2_hybrid.py
@@ -2,19 +2,19 @@ import numpy as np
 import pytest
 
 def Hybrid_norm(f,g, delta):
-#    """
-#    Computer the hibrid l1/l2 norm J = sum f(r) where f(r) = sqrt(1 + (r/delta)^2) - 1 f and g are two seismic images, delta is a positive tuning parameter and r is defined as r = f - g
-#    Parameters
-#    ----------
-#    f, g: function
-#          f and g are the probability distributions to compare
-#    delta: float
-#          positive constant to be chosen
-#    Returns
-#    -------
-#    float
-#    Hibrid l1/l2 norm
-#    """
+    """
+    Computer the hibrid l1/l2 norm J = sum f(r) where f(r) = sqrt(1 + (r/delta)^2) - 1 f and g are two seismic images, delta is a positive tuning parameter and r is defined as r = f - g
+    Parameters
+    ----------
+    f, g: function
+          f and g are the probability distributions to compare
+    delta: float
+          positive constant to be chosen
+    Returns
+    -------
+    float
+    Hibrid l1/l2 norm
+    """
 
     # Calculate the difference between the two seismic images
     r = f - g
