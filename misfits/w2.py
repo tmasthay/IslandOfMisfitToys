5eaa87a0afbc94e52264730b6b26bc280a77f160
tmasthay <tyler@ices.utexas.edu>
2023-05-04 14:34:05 -0500

    Add w2 implementation

--- misfits/w2.py
+++ misfits/w2.py
@@ -1,3 +1,22 @@
-def wass2(f,g,dt,tol=0.0):
-    from .helpers.quantile import smart_quantile_cython as ppf
-    print('wass2')
+from .helpers.quantile.smart_quantile_cython import smart_quantile_peval as ppf
+import numpy as np
+from scipy.integrate import cumulative_trapezoid
+
+def w2_peval(f,dt,tol=0.0):
+    nt = len(f)
+    t = np.array(np.linspace(0.0, (nt-1)*dt, nt), dtype=dtype)
+    Q = ppf(t, f, tol)
+    def aux(g):
+        G = np.array(
+            cumulative_trapezoid(g,dx=dt,initial=0.0), 
+            dtype=np.float32
+        )
+        return np.trapz((Q(G) - t)**2 * g, dx=dt)
+    return aux
+
+def w2(f,g,dt,tol=0.0):
+    u = w2_peval(f,dt,tol)
+    return u(g)
+
+
+    
