d49a41a738f8f9ac43828dd93cd9d9062cdd4127
shenyiran91 <shenyiran91@utexas.edu>
2023-05-07 02:24:42 -0500

    update test_hybrid_norm

--- unit_tests/test_hybrid_norm.py
+++ unit_tests/test_hybrid_norm.py
@@ -21,7 +21,7 @@ def test_hybrid_norm_zero():
     __perform_test(
         f=np.zeros(10),
         g=np.zeros(10),
-        delta=0,
+        delta=1,
         gtruth=lambda a,b,d : 0.0,
         tau=1e-8
     )
@@ -29,17 +29,17 @@ def test_hybrid_norm_zero():
 def test_hybrid_norm_large_difference():
     __perform_test(
         f=np.array([1,2,3]),
-        g=np.array([100,200,300]),
+        g=np.array([1000,2000,3000]),
         delta=2,
-        gtruth=lambda a,b,d : (np.sum(np.abs(a-b)))/d,
-        tau=1e-8
+        gtruth=lambda a,b,d : np.sum(np.abs(a-b)/d),
+        tau=1e+1
     )
 
 def test_hybrid_norm_small_difference():
     __perform_test(
         f=np.array([1,2,3]),
-        g=np.array([2,3,4]),
+        g=np.array([1.1,2.2,3.3]),
         delta=2,
-        gtruth=lambda a,b,d : 0.5 * (np.sum(a-b)/d)**2,
-        tau=1e-8
+        gtruth=lambda a,b,d :np.sum( 0.5 * ((a-b)/d)**2),
+        tau=1e-4
     )
