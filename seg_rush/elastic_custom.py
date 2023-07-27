08d427e1e9873e6545509387df0af72509b87d3f
tyler <tyler@ices.utexas.edu>
2023-07-27 05:17:35 -0500

    Add cuda device options

--- seg_rush/elastic_custom.py
+++ seg_rush/elastic_custom.py
@@ -7,10 +7,14 @@ from deepwave_helpers import *
 import numpy as np
 import argparse
 from random import randint
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 global device
-device = torch.device('cuda' if torch.cuda.is_available()
-                      else 'cpu')
+global cmap
+device = torch.device('cuda:1')
+
+plt.rcParams['text.usetex'] = True
 
 def vertical_stratify(ny, nx, layers, values):
     global device
@@ -19,7 +23,7 @@ def vertical_stratify(ny, nx, layers, values):
     u = values[0] * torch.ones(ny,nx, device=device)
     if( len(layers) > 0 ):
         for l in range(len(layers)-1):
-            u[layers[i]:layers:[i+1], :] = values[i+1]
+            u[layers[l]:layers[l+1], :] = values[l+1]
         u[layers[-1]:] = values[-1]
     return u
 
@@ -28,17 +32,18 @@ def uniform_vertical_stratify(ny, nx, values):
     return vertical_stratify(ny, nx, layers, values)
 
 def plot_material_params(vp, vs, rho):
+    global cmap
+
     up = vp.cpu().detach()
     us = vs.cpu().detach()
     urho = rho.cpu().detach()
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(10,7))
-    axs[0].imshow(up, cmap='seismic')
-    axs[1].imshow(us, cmap='seismic')
-    axs[2].imshow(urho, cmap='seismic')
+    axs[0].imshow(up, cmap=cmap)
+    axs[1].imshow(us, cmap=cmap)
+    axs[2].imshow(urho, cmap=cmap)
 
-    for ax in axs:
-        ax.axis('off')
     plt.savefig('params.jpg')
+    plt.clf()
          
 def get_data(**kw):
     #import global vars
@@ -57,7 +62,7 @@ def get_data(**kw):
         'first_receiver': 0,
         'receiver_depth': 2,
         'd_receiver': 1,
-        'freq': 1.0,
+        'freq': 10.0,
         'nt': 1600,
         'dt': 0.001,
         'ofs': 1
@@ -72,8 +77,8 @@ def get_data(**kw):
 #    d['rho'] = rho_background * torch.ones(d['ny'], d['nx'], device=device)  
 
     ufs = lambda v : uniform_vertical_stratify(d['ny'], d['nx'], v)
-    d['vp'] = ufs([1500.0, 1800.0])
-    d['vs'] = ufs([1100.0, 1300.0])
+    d['vp'] = ufs([1500.0, 1700.0, 1900.0, 2100.0, 3000.0, 2000.0, 1800.0, 1100.0])
+    d['vs'] = ufs([1100.0, 1300.0, 1500.0, 1900.0, 2800.0, 1800.0, 1100.0, 900.0])
     d['rho'] = ufs([2200.0])
 
     plot_material_params(d['vp'], d['vs'], d['rho'])
@@ -83,7 +88,7 @@ def get_data(**kw):
     d['n_receivers_per_shot'] = (d['nx']-1)
 
     #set ricker peak time
-    d['peak_time'] = 0.3 / d['freq']
+    d['peak_time'] = 1.5 / d['freq']
     d['wavelet'] = deepwave.wavelets.ricker(d['freq'],
         d['nt'],
         d['dt'],
@@ -174,10 +179,15 @@ def get_data(**kw):
     u = {**d, **kw}
     return u
 
-def gifify(u, step_size, name, remove_jpg=True):
+def gifify(u, step_size, name, remove_jpg=True, renormalize=True):
+    global cmap
     name = name.replace('.gif', '')
+    kw = {}
+    if( renormalize ):
+        kw['vmin'] = u.max()
+        kw['vmax'] = u.min()
     for k in range(0,u.shape[-1],step_size):
-        plt.imshow(u[:,:,k], cmap='seismic', aspect='auto')
+        plt.imshow(u[:,:,k], cmap=cmap, aspect='auto', **kw)
         plt.title(f'Step {k}')
         plt.colorbar()
         plt.savefig(f'tmp{k}.jpg')
@@ -192,6 +202,8 @@ def forward_samples(sy, sx, d, **kw):
     defaults = {
         'sigy': d['dy'],
         'sigx': d['dx'],
+        'forward': 'forward.pt',
+        'full': 'full'
     }
     curr_kw = {**defaults, **kw}
    
@@ -203,55 +215,147 @@ def forward_samples(sy, sx, d, **kw):
         l,
         d['nt']
     )
-    p_y_idx = randint(0, len(sy))
-    p_x_idx = randint(0, len(sx))
+    norm_image = False
+    p_y_idx = randint(2, len(sy)-2)
+    p_x_idx = randint(2, len(sx)-2)
     for (i,yy) in enumerate(sy):
         for (j,xx) in enumerate(sx):
             full_data = d['forward'](muy=yy, mux=xx, **curr_kw)
             if( i == p_y_idx and j == p_x_idx ):
                 tmp = full_data[0,:,:].reshape(l,m,d['nt']).cpu().detach()
-                gifify(tmp, 100, 'full')
+                gifify(tmp, 100, curr_kw['full'], True, norm_image)
             res[i,j,0,:,:] = full_data[:d['n_shots']][0,:l,:]
-            tmp1 = d['amp_helper']('force_y',
-                muy=yy,
-                mux=xx,
-                **curr_kw
-            )
             print(f'{i},{j}')
     make_receiver_plot = True
     if( d['n_shots'] == 1 and make_receiver_plot ):
-        res_reshaped = res.reshape(len(sy)*len(sx), l, d['nt'])
-        res_reshaped = res_reshaped.permute(1,2,0).cpu()
-        print('Making gif of receiver data')
-        gifify(res_reshaped, 1, 'receiver', True)
+        if( len(sy) * len(sx) < 200 ):
+            res_reshaped = res.reshape(len(sy)*len(sx), l, d['nt'])
+            res_reshaped = res_reshaped.permute(1,2,0).cpu()
+            print('Making gif of receiver data')
+            gifify(res_reshaped, 1, 'receiver', True, norm_image)
     print('Saving data')
-    torch.save(res, 'forward.pt')
+    torch.save(res, curr_kw['forward'])
     return res
 
 def landscape(res, loss, name, **kw):
+    global cmap
     ref_idx = kw.get('ref_idx', [res.shape[0] // 2, res.shape[1] // 2])
     u = res[ref_idx[0], ref_idx[1], 0, :, :] 
     losses = torch.empty(res.shape[0], res.shape[1])
     for i in range(res.shape[0]):
         for j in range(res.shape[1]):
             losses[i,j] = loss(u, res[i,j,0,:,:])
-    plt.imshow(losses)
+    plt.imshow(losses, cmap=cmap)
     plt.colorbar()
     plt.savefig(name)
+
+def landscape_peval(res, loss_builder, name, **kw):
+    global cmap
+    ref_idx = kw.get('ref_idx', [res.shape[0] // 2, res.shape[1] // 2])
+    u = res[ref_idx[0], ref_idx[1], 0, :, :]
+    losses = torch.empty(res.shape[0], res.shape[1])
+    loss_peval = loss_builder(u, **kw)
+    for i in range(res.shape[0]):
+        for j in range(res.shape[1]):
+             losses[i,j] = loss_peval(res[i,j,0,:,:])
+    plt.imshow(losses, cmap=cmap)
+    plt.colorbar()
+    plt.savefig(name) 
+
+def slice2(u, idx):
+    return u[torch.arange(u.shape[0]).unsqueeze(1), idx]
+
+def frac_ss(x, y, tau=1e-16):
+    idx = torch.clamp(torch.searchsorted(x,y), max=x.shape[-1]-1)
+    idx_left = torch.clamp(idx-1, min=0)
+    val_right = slice2(x, idx)
+    val_left = slice2(x, idx_left)
+    dval = torch.max(tau * torch.ones(y.shape).to(val_right.device),
+        val_right - val_left
+    )
+    alpha = (val_right - y) / dval
+    return alpha * idx_left + (1.0 - alpha) * idx
+
+def frac_idx(x, idx):
+    right_idx = torch.ceil(idx).to(torch.long)
+    left_idx = torch.floor(idx).to(torch.long)
+    alpha = idx - torch.floor(idx)
+    x_right = slice2(x, right_idx)
+    x_left = slice2(x, left_idx)
+    return alpha * x_right + (1.0 - alpha) * x_left
+
+def my_quantile(cdf, x, p, tau=1e-16):
+    #cdf -- F(x) for density f supported on x
+    #x -- domain of sample space
+    #p -- quantiles we want to evaluate
+    idx = torch.clamp(torch.searchsorted(cdf, p), max=cdf.shape[-1]-1)
+    idx_left = torch.clamp(idx-1, min=0)
+    cdf_right = slice2(cdf, idx)
+    cdf_left = slice2(cdf, idx_left)
+    dcdf = torch.max(tau * torch.ones(p.shape).to(cdf_right.device),
+        cdf_right - cdf_left
+    )
+    alpha = (cdf_right - p) / dcdf
+    x_left = slice2(x, idx_left)
+    x_right = slice2(x, idx)
+    return alpha * x_left + (1.0 - alpha) * x_right
+
+def my_quantile2(cdf, x, p, tau=1e-16):
+    idx = frac_ss(cdf, p, tau)
+    return frac_idx(x, idx)
    
+def w2_peval(g, **kw): 
+    global device
+    assert( len(g.shape) == 2 )
+
+    x = kw['x'].to(device)
+    dx = x[1] - x[0]
+    num_prob = kw['num_prob']
+    renorm = kw['renorm']
+    x = x.repeat(g.shape[0],1)
+    g = renorm(g).to(device)
+    p = torch.linspace(0.0, 1.0, num_prob).repeat(g.shape[0],1).to(device)
+    G = torch.cumsum(g, dim=1)
+    G = G / G[:,-1].unsqueeze(1)
+    Q = my_quantile2(G, x, p)
+    for (i,qq) in enumerate(Q):
+         fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(11,8))
+         ax[0].plot(x[i].cpu(), g[i].cpu(), label=f'$g_{i}(x)$')
+         ax[0].plot(x[i].cpu(), G[i].cpu(), label=f'$G_{i}(x)$')
+         ax[1].plot(p[i].cpu(), qq, label=f'$Q(p)$')
+         ax[0].legend()
+         ax[1].legend()
+         plt.savefig(f'q{i}.jpg')
+         plt.clf()
+    def helper(f):
+        f = renorm(f).to(device)
+        F = torch.cumsum(f, dim=1)
+        F = F / F[:,-1].unsqueeze(1)
+        transport = frac_idx(x, frac_ss(Q, F)) - x
+        return torch.sum(transport**2 * f) * dx
+    return helper
+
 def go():
     #declare globals
     global device
+    global cmap
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-nsy', type=int, default=10)
-    parser.add_argument('-nsx', type=int, default=10)
-    parser.add_argument('-rerun', action='store_true')
+    parser.add_argument('--nsy', type=int, default=10)
+    parser.add_argument('--nsx', type=int, default=10)
+    parser.add_argument('--norun', action='store_true')
+    parser.add_argument('--misfit', type=str, default='w2')
+    parser.add_argument('--gpu', type=int, default=0)
+    parser.add_argument('--cmap', type=str, default='seismic')
+    parser.add_argument('--forward', type=str, default='forward.pt')
+    parser.add_argument('--full', type=str, default='full')
     args = parser.parse_args()
+    device = torch.device(f'cuda:{args.gpu}')
+    cmap = args.cmap
  
     d = get_data()
 
-    ofs = 1
+    ofs = 30
     y_min = (d['grid_y'][d['first_source']][0] + ofs) * d['dy']
     y_max = (d['grid_y'][-1][0] - ofs) * d['dy']
    
@@ -261,11 +365,28 @@ def go():
     sy = torch.linspace(y_min, y_max, args.nsy).to(device)
     sx = torch.linspace(x_min, x_max, args.nsx).to(device)
 
-    if( args.rerun ):
+    if( not args.norun ):
         forward_samples(sy,sx,d)
     
-    res = torch.load('forward.pt')
-    landscape(res, lambda x,y: torch.norm(x-y)**2, 'L2.jpg')
+    res = torch.load('forward.pt').to(device)
+    if( args.misfit.lower() == 'l2' ):
+        def loss(x,y):
+            dom = max([float(torch.norm(x)**2), float(torch.norm(y)**2)])
+            return torch.norm(x-y)**2 / dom
+        landscape(res, loss, 'L2.jpg')
+    elif( args.misfit.lower() == 'w2' ):
+        t = torch.linspace(0.0, (d['nt']-1)*d['dt'], d['nt'])
+        def renorm(f):
+            assert( len(f.shape) == 2 )
+            u = f**2
+            return u / torch.sum(u,dim=1).unsqueeze(1)
+        landscape_peval(res, 
+            w2_peval, 
+            'W2.jpg', 
+            x=t.to(device),
+            renorm=renorm, 
+            num_prob=100
+        )
 
 if( __name__ == "__main__" ):
     go()
--- seg_rush/tmp.py
+++ seg_rush/tmp.py
@@ -1,2 +1,46 @@
 import torch
-from 
+from elastic_custom import *
+import matplotlib.pyplot as plt
+
+def case1():
+    n = 2000
+    num_prob = 100
+    nex = 2
+    x = torch.linspace(-10,10,n)
+    f = torch.exp(-x**2).repeat(nex,1)
+    F = torch.cumsum(f,dim=1)
+    F = F / F[:,-1].unsqueeze(1)
+    p = torch.linspace(0.0, 1.0, num_prob).repeat(nex,1)
+
+    Q = my_quantile2(F, x.repeat(nex,1), p)
+
+    for ex in range(nex):
+        fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10,7))
+
+        axs[0].plot(x, f[ex], label='pdf')
+        axs[0].plot(x, F[ex], label='cdf')
+        axs[0].legend()
+
+        axs[1].plot(p[ex], Q[ex], label='Q')
+        axs[1].legend()
+
+        plt.savefig(f'tmp{ex}.jpg')
+        plt.clf()
+
+def case2():
+    n = 2000
+    num_prob = 1000
+    nex = 2
+    x = torch.linspace(-10,10,n)
+    f = torch.exp(-x**2).repeat(nex, 1)
+    g = torch.exp(-x**2).repeat(nex,1)
+    def renorm(h):
+       assert( len(h.shape) == 2 )
+       u = h**2
+       return u / torch.sum(u, dim=1).unsqueeze(1) 
+    w2g = w2_peval(g, x=x, renorm=renorm, num_prob=num_prob)
+    res = w2g(f)
+    print(res)
+
+if( __name__ == "__main__" ):
+    case2()
