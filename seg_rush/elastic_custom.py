ea10584688f387317ef9c8b768386b271f5a9674
tyler <tyler@ices.utexas.edu>
2023-07-26 21:02:07 -0500

    Add more plotting mechs

--- seg_rush/elastic_custom.py
+++ seg_rush/elastic_custom.py
@@ -6,11 +6,40 @@ from custom_losses import *
 from deepwave_helpers import *
 import numpy as np
 import argparse
+from random import randint
 
 global device
 device = torch.device('cuda' if torch.cuda.is_available()
                       else 'cpu')
 
+def vertical_stratify(ny, nx, layers, values):
+    global device
+    assert( len(layers) == len(values) - 1 )
+    assert( len(values) >= 1 )
+    u = values[0] * torch.ones(ny,nx, device=device)
+    if( len(layers) > 0 ):
+        for l in range(len(layers)-1):
+            u[layers[i]:layers:[i+1], :] = values[i+1]
+        u[layers[-1]:] = values[-1]
+    return u
+
+def uniform_vertical_stratify(ny, nx, values):
+    layers = [i*ny // len(values) for i in range(1,len(values))]
+    return vertical_stratify(ny, nx, layers, values)
+
+def plot_material_params(vp, vs, rho):
+    up = vp.cpu().detach()
+    us = vs.cpu().detach()
+    urho = rho.cpu().detach()
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(10,7))
+    axs[0].imshow(up, cmap='seismic')
+    axs[1].imshow(us, cmap='seismic')
+    axs[2].imshow(urho, cmap='seismic')
+
+    for ax in axs:
+        ax.axis('off')
+    plt.savefig('params.jpg')
+         
 def get_data(**kw):
     #import global vars
     global device
@@ -35,9 +64,19 @@ def get_data(**kw):
     }
 
     #set material parameters
-    d['vp'] = 1500 * torch.ones(d['ny'], d['nx'], device=device)
-    d['vs'] = 1100 * torch.ones(d['ny'], d['nx'], device=device)
-    d['rho'] = 2200 * torch.ones(d['ny'], d['nx'], device=device)
+#    vp_background = 1500.0
+#    vs_background = 1100.0
+#    rho_background = 2200.0
+#    d['vp'] = vp_background * torch.ones(d['ny'], d['nx'], device=device)
+#    d['vs'] = vs_background * torch.ones(d['ny'], d['nx'], device=device)
+#    d['rho'] = rho_background * torch.ones(d['ny'], d['nx'], device=device)  
+
+    ufs = lambda v : uniform_vertical_stratify(d['ny'], d['nx'], v)
+    d['vp'] = ufs([1500.0, 1800.0])
+    d['vs'] = ufs([1100.0, 1300.0])
+    d['rho'] = ufs([2200.0])
+
+    plot_material_params(d['vp'], d['vs'], d['rho'])
 
     #set per shot numbers
     d['n_sources_per_shot'] = (d['nx']-1)*(d['ny']-1)
@@ -79,7 +118,6 @@ def get_data(**kw):
         local_sigy = kwargs.get('sigy', d['dy'])
         exponent1 = -((x-local_mux)**2 /  (2*local_sigx**2))
         exponent2 = -((y-local_muy)**2 / (2*local_sigy**2))
-        print(f'amp,muy,mux,sigy,sigx={local_amp},{local_muy},{local_mux},{local_sigy},{local_sigx}')
         return local_amp * torch.exp(exponent1 + exponent2)
 
     def my_force_x(y,x, **kwargs):
@@ -137,6 +175,7 @@ def get_data(**kw):
     return u
 
 def gifify(u, step_size, name, remove_jpg=True):
+    name = name.replace('.gif', '')
     for k in range(0,u.shape[-1],step_size):
         plt.imshow(u[:,:,k], cmap='seismic', aspect='auto')
         plt.title(f'Step {k}')
@@ -164,9 +203,14 @@ def forward_samples(sy, sx, d, **kw):
         l,
         d['nt']
     )
+    p_y_idx = randint(0, len(sy))
+    p_x_idx = randint(0, len(sx))
     for (i,yy) in enumerate(sy):
         for (j,xx) in enumerate(sx):
             full_data = d['forward'](muy=yy, mux=xx, **curr_kw)
+            if( i == p_y_idx and j == p_x_idx ):
+                tmp = full_data[0,:,:].reshape(l,m,d['nt']).cpu().detach()
+                gifify(tmp, 100, 'full')
             res[i,j,0,:,:] = full_data[:d['n_shots']][0,:l,:]
             tmp1 = d['amp_helper']('force_y',
                 muy=yy,
@@ -174,11 +218,10 @@ def forward_samples(sy, sx, d, **kw):
                 **curr_kw
             )
             print(f'{i},{j}')
-    make_receiver_plot = False
+    make_receiver_plot = True
     if( d['n_shots'] == 1 and make_receiver_plot ):
         res_reshaped = res.reshape(len(sy)*len(sx), l, d['nt'])
         res_reshaped = res_reshaped.permute(1,2,0).cpu()
-        input(res_reshaped.shape)
         print('Making gif of receiver data')
         gifify(res_reshaped, 1, 'receiver', True)
     print('Saving data')
@@ -186,16 +229,16 @@ def forward_samples(sy, sx, d, **kw):
     return res
 
 def landscape(res, loss, name, **kw):
-    ref_idx = kw.get('ref_idx', res.shape[:2] // 2)
+    ref_idx = kw.get('ref_idx', [res.shape[0] // 2, res.shape[1] // 2])
     u = res[ref_idx[0], ref_idx[1], 0, :, :] 
-    losses = torch.empty(ns, ns)
-    for i in range(ns):
-        for j in range(ns):
-            losses[i,j] = loss(u, v[i,j,0,:,:])
+    losses = torch.empty(res.shape[0], res.shape[1])
+    for i in range(res.shape[0]):
+        for j in range(res.shape[1]):
+            losses[i,j] = loss(u, res[i,j,0,:,:])
     plt.imshow(losses)
     plt.colorbar()
     plt.savefig(name)
-    
+   
 def go():
     #declare globals
     global device
@@ -203,16 +246,17 @@ def go():
     parser = argparse.ArgumentParser()
     parser.add_argument('-nsy', type=int, default=10)
     parser.add_argument('-nsx', type=int, default=10)
-    parser.add_argument('-rerun', type=bool, default=True)
+    parser.add_argument('-rerun', action='store_true')
     args = parser.parse_args()
  
     d = get_data()
 
-    y_min = d['grid_y'][d['first_source']][0] * d['dy']
-    y_max = d['grid_y'][-1][0] * d['dy']
+    ofs = 1
+    y_min = (d['grid_y'][d['first_source']][0] + ofs) * d['dy']
+    y_max = (d['grid_y'][-1][0] - ofs) * d['dy']
    
-    x_min = d['grid_x'][0][0] * d['dx']
-    x_max = d['grid_x'][0][-1] * d['dx']
+    x_min = (d['grid_x'][0][0] + ofs) * d['dx']
+    x_max = (d['grid_x'][0][-1] - ofs) * d['dx']
 
     sy = torch.linspace(y_min, y_max, args.nsy).to(device)
     sx = torch.linspace(x_min, x_max, args.nsx).to(device)
