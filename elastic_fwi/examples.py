48913a0203fd5618fb95d6aa4118fcb5224c7019
tmasthay <tyler@ices.utexas.edu>
2023-08-21 18:20:49 -0500

    Add trace plotting check -- works

--- elastic_fwi/examples.py
+++ elastic_fwi/examples.py
@@ -78,23 +78,111 @@ def get_plot_config(u, args):
     setup_gg_plot(clr_out='black', clr_in='black')
     return kw, open_plot, config_plot
 
+def plot_2d(
+    *,
+    vals, 
+    title, 
+    name, 
+    config_plot, 
+    open_plot, 
+    xlabel='',
+    ylabel='',
+    **kw
+):
+    name = name.replace('.jpg', '')
+    plt.imshow(vals, **kw)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    config_plot(title)
+    plt.savefig('%s.jpg'%name)
+    plt.clf()
+    open_plot('%s.jpg'%name)
+
+def plot_1d_sequence(
+    *,
+    vals, 
+    title, 
+    name, 
+    config_plot, 
+    open_plot, 
+    dt,
+    dx,
+    xlabel='',
+    ylabel='',
+    step=1,
+    **kw,
+):
+    name = name.replace('.jpg', '')
+    t = [i * dt for i in range(0, len(vals[0]))]
+    nx = vals.shape[0]
+    for i in range(0, nx, step):
+        print('Plotting %d of %d'%(i // step, nx // step))
+        plt.plot(t, vals[i,:], **kw)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        config_plot('%s x=%d'%(title, i*dx))
+        plt.savefig('%s_%d.jpg'%(name, i))
+        plt.clf()
+    os.system(f'convert -delay 10 $(ls -tr {name}_*.jpg) {name}.gif')
+    os.system(f'rm {name}_*.jpg')
+    open_plot(f'{name}.gif')
+
+def plot_results(
+    *, 
+    displacement, 
+    data, 
+    config_plot,
+    open_plot,
+    dt,
+    dx,
+    step=1,
+    **kw
+):
+    def helper(*, func, **kw_local):
+        func(open_plot=open_plot, config_plot=config_plot, **kw_local)
+
+    helper(
+        func=plot_2d,
+        vals=displacement[0],
+        title='Time offset',
+        name='u_time_offset',
+        xlabel='Horizontal location (km)',
+        ylabel='Depth (km)',
+        **kw
+    )
+    helper(
+        func=plot_2d,
+        vals=data.vp.cpu(),
+        title=r'$V_p$',
+        name='vp',
+        xlabel='Horizontal location (km)',
+        ylabel='Depth (km)',
+        **kw
+    )
+    plot_1d_sequence(
+        vals=displacement[0],
+        title='Trace',
+        name='u_trace',
+        xlabel='Time (s)',
+        ylabel='Amplitude',
+        dt=dt,
+        dx=dx,
+        open_plot=open_plot,
+        config_plot=(lambda x: plt.title(x)),
+        step=step
+    )
+
 if( __name__ == '__main__' ):
     args = get_cmd_line_args()
     u, data = get_data(marmousi_real, args)
     plot_kw, open_plot, config_plot = get_plot_config(u, args)
-
-    if( args.mode == 'time_offset' ):
-        plt.imshow(u[0], cmap=args.cmap, aspect='auto')
-        config_plot('Time offset')
-        plt.savefig("u_time_offset.jpg")
-        plt.clf()
-        open_plot('u_time_offset.jpg')
-    elif( args.mode == 'traces' or args.mode == 'trace' ):
-        plt.imshow(u[0], cmap=args.cmap, aspect='auto')
-        config_plot('Time offset')
-        plt.savefig("u_trace.jpg")
-        plt.clf()
-        open_plot('u_trace.jpg')
-    else:
-        print(f'Mode "{args.mode}" not supported')
-    print('DONE')
+    plot_results(
+        displacement=u,
+        data=data,
+        config_plot=config_plot,
+        open_plot=open_plot,
+        dt=data.dt,
+        dx=data.dx,
+        step=100,
+        **plot_kw
+    )
