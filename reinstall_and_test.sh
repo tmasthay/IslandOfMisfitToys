a9f079cca5beed7c274e730fa8b2f305aa6916b8
tmasthay <tyler@ices.utexas.edu>
2023-08-22 17:43:17 -0500

    Update script -- works from clean env

--- reinstall_and_test.sh
+++ reinstall_and_test.sh
@@ -1,7 +1,6 @@
 python update_imports.py
 pip uninstall -y IslandOfMisfitToys
-python setup.py sdist bdist_wheel
-pip install dist/IslandOfMisfitToys-0.2.1-py2.py3-none-any.whl
+pip install .
 
 CURR=$(pwd)
 cd
