169169c39e0ab45d2809285701a50f007730c0ef
tmasthay <tyler@ices.utexas.edu>
2023-08-25 18:05:15 -0500

    Fix geophone_curtin path name

--- misfit_toys/data/convert.py
+++ misfit_toys/data/convert.py
@@ -173,7 +173,7 @@ def main():
             'url': 'https://ddfe.curtin.edu.au/7h0e-d392/',
             'ext': 'sgy',
             'das_curtin': {'filename': '2020_GeoLab_WVSP_DAS_wgm.sgy'},
-            'geophone_curtin.sgy': {
+            'geophone_curtin': {
                 'filename': '2020_GeoLab_WVSP_geophone_wgm.sgy'
             },
         }
