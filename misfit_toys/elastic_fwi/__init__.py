0e507f61980af1bd880dc9fd265a38edc84120c6
tmasthay <tyler@ices.utexas.edu>
2023-08-25 21:55:52 -0500

    Get rid of debugging script that messes up __init__ structure

--- misfit_toys/elastic_fwi/__init__.py
+++ misfit_toys/elastic_fwi/__init__.py
@@ -5,7 +5,6 @@ __all__ = [
     "elastic_custom",
     "seismic_data",
     "postprocess",
-    "delete_this",
     "elastic_class"
 ]
 from .examples import *
@@ -14,5 +13,4 @@ from .custom_losses import *
 from .elastic_custom import *
 from .seismic_data import *
 from .postprocess import *
-from .delete_this import *
 from .elastic_class import *
