326b2002d09f865d26409a840c12280a4bd547cf
tmasthay <tyler@ices.utexas.edu>
2023-08-28 18:49:33 -0500

    BUILD: update __init__ file structure

--- misfit_toys/elastic_fwi/__init__.py
+++ misfit_toys/elastic_fwi/__init__.py
@@ -1,22 +1,16 @@
 __all__ = [
-    "examples",
     "custom_losses",
     "test_hierarchy",
     "driver",
-    "examples_refactor",
     "elastic_custom",
     "seismic_data",
     "postprocess",
-    "examples_intended_final",
     "elastic_class"
 ]
-from .examples import *
 from .custom_losses import *
 from .test_hierarchy import *
 from .driver import *
-from .examples_refactor import *
 from .elastic_custom import *
 from .seismic_data import *
 from .postprocess import *
-from .examples_intended_final import *
 from .elastic_class import *
