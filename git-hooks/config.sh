2532a6776b94cdc4d9833532e07a686e78d2371a
tmasthay <tyler@ices.utexas.edu>
2023-09-12 22:48:49 -0500

    CONFIG: Add pathing checks -- should work now

--- git-hooks/config.sh
+++ git-hooks/config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -z "$ISL" ]; then
+    echo "Error: Need to set ISL environment variable for this script to work"
+    exit 1
+fi
+
 # Define the directories
 CURRENT_DIR="$ISL/git-hooks"
-HOOKS_DIR="../.git/hooks"
+HOOKS_DIR="$ISL/.git/hooks"
 
 # Create the symbolic link
 ln -s -f "$CURRENT_DIR/commit-msg" "$HOOKS_DIR/commit-msg"
