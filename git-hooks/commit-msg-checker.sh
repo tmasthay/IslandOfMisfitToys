151d49fdb698e903e720a16fe42fde792f43b670
tmasthay <tyler@ices.utexas.edu>
2023-09-12 21:04:40 -0500

    CONFIG: Add commit message enforcement and auto-config file

---
+++ git-hooks/commit-msg-checker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COMMIT_MSG_FILE="$1"
+DIRECTIVES="BUG|BUGFIX|FEATURE|DEBUG|CLEAN|DOCS|SAFETY|REFACTOR|STYLE|TEST|INIT|CONFIG|REVERT|DEPRECATE|UPDATE|CHORE|PERFORMANCE"
+PATTERN="^($DIRECTIVES): .+"
+
+# Check the commit message for patterns
+if ! grep -qE "$PATTERN" "$COMMIT_MSG_FILE"; then
+    echo "Error: Commit message does not follow the required format."
+    echo "Please start your commit message with one of the following directives followed by a colon and a description:"
+    echo "$DIRECTIVES"
+    exit 1
+fi
+
---
+++ git-hooks/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the directories
+CURRENT_DIR=$(dirname "$0")
+HOOKS_DIR="../.git/hooks"
+
+# Create the symbolic link
+ln -s -f "$CURRENT_DIR/commit-msg" "$HOOKS_DIR/commit-msg"
+
+# Provide feedback
+if [ $? -eq 0 ]; then
+    echo "Symbolic link created successfully!"
+else
+    echo "Failed to create symbolic link."
+fi
+
