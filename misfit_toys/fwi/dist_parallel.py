3edc5ccf7b669ca6697f90c62c2de359efbb8672
tmasthay <tyler@ices.utexas.edu>
2023-09-16 20:05:49 -0500

    REFACTOR: abstract out training

--- misfit_toys/fwi/dist_parallel.py
+++ misfit_toys/fwi/dist_parallel.py
@@ -82,6 +82,38 @@ def get_initials(*, ny, nx, filter_freq, n_shots, rec_per_shot, nt):
 
     return v_true, v_init, obs_data
 
+def train(*, prop, src_amp, src_loc, rec_loc, obs_data, dt, rank):
+    # Setup optimiser to perform inversion
+    loss_fn = torch.nn.MSELoss()
+
+    # Run optimisation/inversion
+    n_epochs = 2
+
+    for cutoff_freq in [10, 15, 20, 25, 30]:
+        sos = butter(6, cutoff_freq, fs=1/dt, output='sos')
+        sos = [torch.tensor(sosi).to(obs_data.dtype).to(rank) for sosi in sos]
+
+        def filt(x):
+            return biquad(biquad(biquad(x, *sos[0]), *sos[1]),
+                          *sos[2])
+        observed_data_filt = filt(obs_data)
+        optimiser = torch.optim.LBFGS(prop.parameters())
+        for epoch in range(n_epochs):
+            def closure():
+                optimiser.zero_grad()
+                out = prop(src_amp, src_loc, rec_loc)
+                out_filt = filt(taper(out[-1]))
+                loss = 1e6*loss_fn(out_filt, observed_data_filt)
+                print(
+                    f'Rank={rank}, Freq={cutoff_freq}, Epoch={epoch}, ' +
+                    f'Loss={loss.item():.4e}',
+                    flush=True
+                )
+                loss.backward()
+                return loss
+
+            optimiser.step(closure)
+
 def run_rank(rank, world_size):
     print(f"Running DDP on rank {rank} / {world_size}.")
     setup(rank, world_size)
@@ -153,36 +185,15 @@ def run_rank(rank, world_size):
     prop = Prop(model, dx, dt, freq).to(rank)
     prop = DDP(prop, device_ids=[rank])
 
-    # Setup optimiser to perform inversion
-    loss_fn = torch.nn.MSELoss()
-
-    # Run optimisation/inversion
-    n_epochs = 2
-
-    for cutoff_freq in [10, 15, 20, 25, 30]:
-        sos = butter(6, cutoff_freq, fs=1/dt, output='sos')
-        sos = [torch.tensor(sosi).to(obs_data.dtype).to(rank) for sosi in sos]
-
-        def filt(x):
-            return biquad(biquad(biquad(x, *sos[0]), *sos[1]),
-                          *sos[2])
-        observed_data_filt = filt(obs_data)
-        optimiser = torch.optim.LBFGS(prop.parameters())
-        for epoch in range(n_epochs):
-            def closure():
-                optimiser.zero_grad()
-                out = prop(src_amp, src_loc, rec_loc)
-                out_filt = filt(taper(out[-1]))
-                loss = 1e6*loss_fn(out_filt, observed_data_filt)
-                print(
-                    f'Rank={rank}, Freq={cutoff_freq}, Epoch={epoch}, ' +
-                    f'Loss={loss.item():.4e}',
-                    flush=True
-                )
-                loss.backward()
-                return loss
-
-            optimiser.step(closure)
+    train(
+        prop=prop,
+        src_amp=src_amp,
+        src_loc=src_loc,
+        rec_loc=rec_loc,
+        obs_data=obs_data,
+        dt=dt,
+        rank=rank
+    )
 
     # Plot
     if rank == 0:
