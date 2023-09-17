3735ef8e06c82cb33bdef27f9f55e53efab63e34
tmasthay <tyler@ices.utexas.edu>
2023-09-17 08:01:25 -0500

    REFACTOR: Move survey into SeismicData
    STATUS: driver still works fine

--- misfit_toys/fwi/dist_parallel.py
+++ misfit_toys/fwi/dist_parallel.py
@@ -23,51 +23,52 @@ def run_rank(rank, world_size):
     data = SeismicData()
 
     #source locations
-    src_loc = towed_src(
-        n_shots=data.n_shots,
-        src_per_shot=data.src_per_shot,
-        d_src=20,
-        fst_src=10,
-        src_depth=2,
-        d_intra_shot=0
-    )
+    # src_loc = towed_src(
+    #     n_shots=data.n_shots,
+    #     src_per_shot=data.src_per_shot,
+    #     d_src=20,
+    #     fst_src=10,
+    #     src_depth=2,
+    #     d_intra_shot=0
+    # )
 
-    #receiver locations
-    rec_loc = fixed_rec(
-        n_shots=data.n_shots,
-        rec_per_shot=data.rec_per_shot,
-        d_rec=6,
-        rec_depth=2,
-        fst_rec=0
-    )
+    # #receiver locations
+    # rec_loc = fixed_rec(
+    #     n_shots=data.n_shots,
+    #     rec_per_shot=data.rec_per_shot,
+    #     d_rec=6,
+    #     rec_depth=2,
+    #     fst_rec=0
+    # )
 
-    # source amplitudes
-    src_amp = (
-        (dw.wavelets.ricker(data.freq, data.nt, data.dt, data.peak_time))
-        .repeat(data.n_shots, data.src_per_shot, 1)
-    )
+    # # source amplitudes
+    # src_amp = (
+    #     (dw.wavelets.ricker(data.freq, data.nt, data.dt, data.peak_time))
+    #     .repeat(data.n_shots, data.src_per_shot, 1)
+    # )
 
     model = Model(data.v_init, 1000, 2500)
 
     #Setup distribution onto multiple GPUs
     d = Distribution(rank=rank, world_size=world_size)
-    prop, data.obs_data, src_amp, src_loc, rec_loc = d.setup_distribution(
-        obs_data=data.obs_data,
-        src_amp=src_amp,
-        src_loc=src_loc,
-        rec_loc=rec_loc,
-        model=model,
-        dx=data.dx,
-        dt=data.dt,
-        freq=data.freq
-    )
+    prop, data.obs_data, data.src_amp_y, data.src_loc, data.rec_loc = \
+        d.setup_distribution(
+            obs_data=data.obs_data,
+            src_amp=data.src_amp_y,
+            src_loc=data.src_loc,
+            rec_loc=data.rec_loc,
+            model=model,
+            dx=data.dx,
+            dt=data.dt,
+            freq=data.freq
+        )
 
     #Perform training
     train_obj = Training(
         prop=prop,
-        src_amp=src_amp,
-        src_loc=src_loc,
-        rec_loc=rec_loc,
+        src_amp=data.src_amp_y,
+        src_loc=data.src_loc,
+        rec_loc=data.rec_loc,
         obs_data=data.obs_data,
         dt=data.dt,
         rank=rank
--- misfit_toys/fwi/modules/seismic_data.py
+++ misfit_toys/fwi/modules/seismic_data.py
@@ -1,22 +1,28 @@
-from torchaudio.functional import biquad
 from ...utils import *
+from torchaudio.functional import biquad
 from typing import Annotated as Ant, Optional as Opt
 from scipy.ndimage import gaussian_filter
 
 class SeismicData:
     def __init__(self):
+        self.define_parameters()
+        self.get_initials()
+        self.build_survey()
+
+    def define_parameters(self):
         self.ny, self.nx, self.nt = 600, 250, 300
         self.dy, self.dx, self.dt = 4.0, 4.0, 0.004
 
         self.n_shots, self.src_per_shot, self.rec_per_shot = 16, 1, 100
+        self.d_src, self.fst_src, self.src_depth = 20, 10, 2
+        self.d_rec, self.fst_rec, self.rec_depth = 6, 0, 2
+        self.d_intra_shot = 0
 
         self.freq = 25
         self.peak_time = 1.5 / self.freq
 
         self.taper_length = 100
         self.filter_freq = 40
-
-        self.get_initials()
 
     def get_initials(self):
         #grab marmousi data
@@ -37,4 +43,29 @@ class SeismicData:
         self.v_init = torch.tensor(
             1/gaussian_filter(1/self.v_true.numpy(), self.filter_freq)
         )
+
+    def build_survey(self):
+        self.src_loc = towed_src(
+            n_shots=self.n_shots,
+            src_per_shot=self.src_per_shot,
+            d_src=self.d_src,
+            fst_src=self.fst_src,
+            src_depth=self.src_depth,
+            d_intra_shot=self.d_intra_shot
+        )
+
+        #receiver locations
+        self.rec_loc = fixed_rec(
+            n_shots=self.n_shots,
+            rec_per_shot=self.rec_per_shot,
+            d_rec=self.d_rec,
+            rec_depth=self.rec_depth,
+            fst_rec=self.fst_rec
+        )
+
+        # source amplitudes
+        self.src_amp_y = \
+            dw.wavelets.ricker(self.freq, self.nt, self.dt, self.peak_time) \
+            .repeat(self.n_shots, self.src_per_shot, 1)
+    # )
 
