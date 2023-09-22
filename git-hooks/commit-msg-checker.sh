3b6ed173aae426ce8cd917a6fe74854eb333c84c
tmasthay <tyler@ices.utexas.edu>
2023-09-21 19:02:45 -0500

    CLEAN: Update commit message enforcement rules

--- git-hooks/commit-msg-checker.sh
+++ git-hooks/commit-msg-checker.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
 COMMIT_MSG_FILE="$1"
-DIRECTIVES="BUG|BUGFIX|FEATURE|DEBUG|CLEAN|DOCS|SAFETY|REFACTOR|REFACTOR_CKPT|STYLE|TEST|INIT|CONFIG|REVERT|DEPRECATE|UPDATE|CHORE|PERFORMANCE"
-PATTERN="^($DIRECTIVES): .+"
 
+# Define directives as an array
+DIRECTIVES=(
+    "BUG"
+    "BUGFIX"
+    "FEATURE"
+    "DEBUG"
+    "CLEAN"
+    "DOCS"
+    "SAFETY"
+    "REFACTOR"
+    "REFACTOR_CKPT"
+    "TEST"
+    "CONFIG"
+    "REVERT"
+    "DEPRECATE"
+    "UPDATE"
+    "PERFORMANCE"
+    "PERF"
+    "DROP"
+    "REQUEST"
+    "PAUSE_BUG"
+    "PAUSE_DOCS"
+    "PAUSE_FEATURE"
+    "PAUSE_PERF"
+    "PAUSE_PERFORMANCE"
+    "PAUSE_OVERKILL"
+)
+
+# Join array elements into a string pattern
+PATTERN="^($(IFS="|"; echo "${DIRECTIVES[*]}")): .+"
+
+# Check the commit message for patterns
 # Check the commit message for patterns
 if ! grep -qE "$PATTERN" "$COMMIT_MSG_FILE"; then
     echo "Error: Commit message does not follow the required format."
     echo "Please start your commit message with one of the following directives followed by a colon and a description:"
-    echo "$DIRECTIVES"
+    IFS=$'\n' # Change IFS to newline for this operation
+    printf '    %s\n' "${DIRECTIVES[@]}" 
+    echo "Example:"
+    echo "    FEATURE: Add new feature"
+    echo "***COMMIT FAILED: SEE USAGE ABOVE***"
     exit 1
 fi
 
