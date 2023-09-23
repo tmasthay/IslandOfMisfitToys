9a603c2c3d2e21ffc0d2e2cb1a298ea3f3574651
tmasthay <tyler@ices.utexas.edu>
2023-09-23 17:46:34 -0500

    REFACTOR: Safety before ddp_deepwave gets class restructure

--- examples/ddp/ddp_deepwave.py
+++ examples/ddp/ddp_deepwave.py
@@ -1,4 +1,9 @@
-from misfit_toys.data.dataset import field_getter, field_saver, get_data3, fetch_warn
+from misfit_toys.data.dataset import (
+    field_getter,
+    field_saver,
+    get_data3,
+    fetch_warn,
+)
 
 import os
 import torch
@@ -35,14 +40,14 @@ class Model(torch.nn.Module):
         self.min_vel = min_vel
         self.max_vel = max_vel
         self.model = torch.nn.Parameter(
-            torch.logit((initial - min_vel) /
-                        (max_vel - min_vel))
+            torch.logit((initial - min_vel) / (max_vel - min_vel))
         )
 
     def forward(self):
-        return (torch.sigmoid(self.model) *
-                (self.max_vel - self.min_vel) +
-                self.min_vel)
+        return (
+            torch.sigmoid(self.model) * (self.max_vel - self.min_vel)
+            + self.min_vel
+        )
 
 
 class Prop(torch.nn.Module):
@@ -53,11 +58,12 @@ class Prop(torch.nn.Module):
         self.dt = dt
         self.freq = freq
 
-    def forward(self, source_amplitudes, source_locations,
-                receiver_locations):
+    def forward(self, source_amplitudes, source_locations, receiver_locations):
         v = self.model()
         return scalar(
-            v, self.dx, self.dt,
+            v,
+            self.dx,
+            self.dt,
             source_amplitudes=source_amplitudes,
             source_locations=source_locations,
             receiver_locations=receiver_locations,
@@ -66,17 +72,18 @@ class Prop(torch.nn.Module):
             time_pad_frac=0.2,
         )
 
+
 def generate_data(rank, world_size):
     print(f"Running DDP on rank {rank} / {world_size}.")
-    setup(rank, world_size)
     ny = 2301
     nx = 751
     dx = 4.0
 
     has_vp_bin = True
     try:
-        v_true = torch.from_file('marmousi_vp.bin',
-                             size=ny*nx).reshape(ny, nx)
+        v_true = torch.from_file('marmousi_vp.bin', size=ny * nx).reshape(
+            ny, nx
+        )
     except:
         fetch_warn()
         has_vp_bin = False
@@ -89,7 +96,7 @@ def generate_data(rank, world_size):
     v_true = v_true[:ny, :nx]
 
     # Smooth to use as starting model
-    v_init = torch.tensor(1/gaussian_filter(1/v_true.numpy(), 40))
+    v_init = torch.tensor(1 / gaussian_filter(1 / v_true.numpy(), 40))
 
     n_shots = 115
 
@@ -109,16 +116,14 @@ def generate_data(rank, world_size):
     peak_time = 1.5 / freq
 
     try:
-        observed_data = (
-            torch.from_file('marmousi_data.bin',
-                            size=n_shots*n_receivers_per_shot*nt)
-            .reshape(n_shots, n_receivers_per_shot, nt)
-        )
+        observed_data = torch.from_file(
+            'marmousi_data.bin', size=n_shots * n_receivers_per_shot * nt
+        ).reshape(n_shots, n_receivers_per_shot, nt)
     except:
-        if( has_vp_bin ):
+        if has_vp_bin:
             raise ValueError(
                 'See code...has should be impossible '
-                    'when trying to fetch observed_data'
+                'when trying to fetch observed_data'
             )
         fetch_warn()
         observed_data = get('obs_data')
@@ -131,41 +136,38 @@ def generate_data(rank, world_size):
     n_shots = 16
     n_receivers_per_shot = 100
     nt = 300
-    observed_data = (
-        taper(observed_data[:n_shots, :n_receivers_per_shot, :nt])
-    )
+    observed_data = taper(observed_data[:n_shots, :n_receivers_per_shot, :nt])
 
     # source_locations
-    source_locations = torch.zeros(n_shots, n_sources_per_shot, 2,
-                                   dtype=torch.long)
+    source_locations = torch.zeros(
+        n_shots, n_sources_per_shot, 2, dtype=torch.long
+    )
     source_locations[..., 1] = source_depth
-    source_locations[:, 0, 0] = (torch.arange(n_shots) * d_source +
-                                 first_source)
+    source_locations[:, 0, 0] = torch.arange(n_shots) * d_source + first_source
 
     # receiver_locations
-    receiver_locations = torch.zeros(n_shots, n_receivers_per_shot,
-                                     2, dtype=torch.long)
+    receiver_locations = torch.zeros(
+        n_shots, n_receivers_per_shot, 2, dtype=torch.long
+    )
     receiver_locations[..., 1] = receiver_depth
     receiver_locations[:, :, 0] = (
-        (torch.arange(n_receivers_per_shot) * d_receiver +
-         first_receiver)
-        .repeat(n_shots, 1)
-    )
+        torch.arange(n_receivers_per_shot) * d_receiver + first_receiver
+    ).repeat(n_shots, 1)
 
     # source_amplitudes
     source_amplitudes = (
-        (deepwave.wavelets.ricker(freq, nt, dt, peak_time))
-        .repeat(n_shots, n_sources_per_shot, 1)
-    )
+        deepwave.wavelets.ricker(freq, nt, dt, peak_time)
+    ).repeat(n_shots, n_sources_per_shot, 1)
 
-    observed_data = \
-        torch.chunk(observed_data, world_size)[rank].to(rank)
-    source_amplitudes = \
-        torch.chunk(source_amplitudes, world_size)[rank].to(rank)
-    source_locations = \
-        torch.chunk(source_locations, world_size)[rank].to(rank)
-    receiver_locations = \
-        torch.chunk(receiver_locations, world_size)[rank].to(rank)
+    print(f'Rank={rank}, id={id(observed_data)}', flush=True)
+    observed_data = torch.chunk(observed_data, world_size)[rank].to(rank)
+    source_amplitudes = torch.chunk(source_amplitudes, world_size)[rank].to(
+        rank
+    )
+    source_locations = torch.chunk(source_locations, world_size)[rank].to(rank)
+    receiver_locations = torch.chunk(receiver_locations, world_size)[rank].to(
+        rank
+    )
 
     model = Model(v_init, 1000, 2500)
     prop = Prop(model, dx, dt, freq).to(rank)
@@ -181,28 +183,31 @@ def generate_data(rank, world_size):
 
     vp_record = torch.zeros(len(freqs), n_epochs, *v_true.shape)
     print(f'TRAIN BEGIN, Rank={rank}', flush=True)
-    for (idx, cutoff_freq) in enumerate(freqs):
-        sos = butter(6, cutoff_freq, fs=1/dt, output='sos')
-        sos = [torch.tensor(sosi).to(observed_data.dtype).to(rank)
-               for sosi in sos]
+    for idx, cutoff_freq in enumerate(freqs):
+        sos = butter(6, cutoff_freq, fs=1 / dt, output='sos')
+        sos = [
+            torch.tensor(sosi).to(observed_data.dtype).to(rank) for sosi in sos
+        ]
 
         def filt(x):
-            return biquad(biquad(biquad(x, *sos[0]), *sos[1]),
-                          *sos[2])
+            return biquad(biquad(biquad(x, *sos[0]), *sos[1]), *sos[2])
+
         observed_data_filt = filt(observed_data)
         optimiser = torch.optim.LBFGS(prop.parameters())
-        epoch_loss = 0.0
-        closure_calls = 0
         for epoch in range(n_epochs):
+            closure_calls = 0
+            epoch_loss = 0.0
+
             def closure():
                 nonlocal closure_calls, epoch_loss
                 closure_calls += 1
                 optimiser.zero_grad()
-                out = prop(source_amplitudes, source_locations,
-                           receiver_locations)
+                out = prop(
+                    source_amplitudes, source_locations, receiver_locations
+                )
                 out_filt = filt(taper(out[-1]))
-                loss = 1e6*loss_fn(out_filt, observed_data_filt)
-                if( closure_calls == 1 ):
+                loss = 1e6 * loss_fn(out_filt, observed_data_filt)
+                if closure_calls == 1:
                     epoch_loss = loss.item()
                 loss.backward()
                 return loss
@@ -210,14 +215,20 @@ def generate_data(rank, world_size):
             optimiser.step(closure)
             vp_record[idx, epoch] = model().detach().cpu()
             print(
-                f'Loss={epoch_loss:.16f}, '
+                (
+                    f'Loss={epoch_loss:.16f}, '
                     f'Freq={cutoff_freq}, '
                     f'Epoch={epoch}, '
-                    f'Rank={rank}',
-                flush=True
+                    f'Rank={rank}'
+                ),
+                flush=True,
             )
     print(f'TRAIN END, Rank={rank}', flush=True)
     torch.save(torch.Tensor(freqs), f'deepwave/data/freqs.pt')
+    torch.save(vp_record, f'deepwave/data/vp_record.pt')
+    torch.save(v_init, f'deepwave/data/vp_init.pt')
+    torch.save(v_true, f'deepwave/data/vp_true.pt')
+
 
 def run_rank(rank, world_size):
     setup(rank, world_size)
@@ -225,63 +236,65 @@ def run_rank(rank, world_size):
     fig_save = 'deepwave/figs'
 
     output_files = [
-        f'{data_save}/{e}.pt' 
-            for e in ['vp_record', 'vp_init', 'vp_true', 'freqs']
+        f'{data_save}/{e}.pt'
+        for e in ['vp_record', 'vp_init', 'vp_true', 'freqs']
     ]
-    if( not all([os.path.exists(f) for f in output_files]) ):
+    if not all([os.path.exists(f) for f in output_files]):
         generate_data(rank, world_size)
     else:
-        if( rank == 0 ):
+        if rank == 0:
             print(
-                'Skipping data generation, delete pt files in {data_save} '
-                    'and re-run this script to regenerate', 
-                flush=True
+                (
+                    'Skipping data generation, delete pt files in {data_save} '
+                    'and re-run this script to regenerate'
+                ),
+                flush=True,
             )
-    
+
     torch.distributed.barrier()
     print(f'LOAD BEGIN, Path={data_save}/vp_record.pt, Rank={rank}', flush=True)
     vp_record = torch.load(f'{data_save}/vp_record.pt')
     v_init = torch.load(f'{data_save}/vp_init.pt')
     v_true = torch.load(f'{data_save}/vp_true.pt')
     freqs = torch.load(f'{data_save}/freqs.pt')
-    
+
     # Plot
     if rank == 0:
         torch.save(vp_record, f'{data_save}/vp_record.pt')
         torch.save(v_init, f'{data_save}/vp_init.pt')
         torch.save(v_true, f'{data_save}/vp_true.pt')
- 
+
         v = vp_record[-1, -1, :, :]
         vmin = v_true.min()
         vmax = v_true.max()
         cmap = 'gray'
-        _, ax = plt.subplots(3, figsize=(10.5, 10.5), sharex=True,
-                             sharey=True)
-        ax[0].imshow(v.T, aspect='auto', cmap=cmap,
-                     vmin=vmin, vmax=vmax)
+        _, ax = plt.subplots(3, figsize=(10.5, 10.5), sharex=True, sharey=True)
+        ax[0].imshow(v.T, aspect='auto', cmap=cmap, vmin=vmin, vmax=vmax)
         ax[0].set_title("Initial")
-        ax[1].imshow(v.T, aspect='auto', cmap=cmap,
-                     vmin=vmin, vmax=vmax)
+        ax[1].imshow(v.T, aspect='auto', cmap=cmap, vmin=vmin, vmax=vmax)
         ax[1].set_title("Out")
-        ax[2].imshow(v_true.T, aspect='auto', cmap=cmap,
-                     vmin=vmin, vmax=vmax)
+        ax[2].imshow(v_true.T, aspect='auto', cmap=cmap, vmin=vmin, vmax=vmax)
         ax[2].set_title("True")
         plt.tight_layout()
         plt.savefig(f'{fig_save}/deepwave_ddp_final_inversion.jpg')
 
-        for freq_idx,epoch in prod(
-            range(vp_record.shape[0]), 
-            range(vp_record.shape[1])
+        for freq_idx, epoch in prod(
+            range(vp_record.shape[0]), range(vp_record.shape[1])
         ):
-            ax[1].imshow(vp_record[freq_idx,epoch].cpu().T, aspect='auto', cmap=cmap,
-                       vmin=vmin, vmax=vmax)
+            ax[1].imshow(
+                vp_record[freq_idx, epoch].cpu().T,
+                aspect='auto',
+                cmap=cmap,
+                vmin=vmin,
+                vmax=vmax,
+            )
             ax[1].set_title(f"Epoch {epoch}, cutoff freq {freqs[freq_idx]}")
             plt.savefig(f'{fig_save}/deepwave_ddp_{freq_idx}_{epoch}.jpg')
-        
+
         dw_iters = f'{fig_save}/deepwave_ddp_[0-9]*_[0-9]*.jpg'
         cmd = (
-            f'convert -delay 100 -loop 0 '
-                f'$(ls -tr {dw_iters}) {fig_save}/deepwave_ddp.gif'
+            'convert -delay 100 -loop 0 '
+            f'$(ls -tr {dw_iters}) {fig_save}/deepwave_ddp.gif'
         )
         print('GIF creation attempt')
         print(f'    cmd="{cmd}"')
@@ -293,11 +306,7 @@ def run_rank(rank, world_size):
 
 
 def run(world_size):
-
-    mp.spawn(run_rank,
-             args=(world_size,),
-             nprocs=world_size,
-             join=True)
+    mp.spawn(run_rank, args=(world_size,), nprocs=world_size, join=True)
 
 
 if __name__ == "__main__":
--- examples/example.py
+++ examples/example.py
@@ -1,90 +1,250 @@
-from misfit_toys.modules.distribution import cleanup, setup
+from misfit_toys.fwi.modules.distribution import cleanup, setup
 
 from abc import ABC, abstractmethod
 import os
 import torch
+import torch.multiprocessing as mp
+import matplotlib.pyplot as plt
+from warnings import warn
+
 
 class Example(ABC):
-    def __init__(self, *, data_save, fig_save, tensor_names, **kw):
+    def __init__(self, *, data_save, fig_save, tensor_names, verbose=1, **kw):
         self.data_save = data_save
         self.fig_save = fig_save
         self.tensor_names = tensor_names
-        self.output_files = [f'{data_save}/{e}.pt' for e in tensor_names]
-        if( 'output_files' in kw.keys() ):
+        self.output_files = {
+            e: os.path.join(data_save, f'{e}.pt') for e in tensor_names
+        }
+        self.verbose = verbose
+        self.tensors = {}
+        if 'output_files' in kw.keys():
             raise ValueError(
                 'output_files is generated from tensor_names'
-                    'and thus should be treated like a reserved keyword'
+                'and thus should be treated like a reserved keyword'
             )
         self.__dict__.update(kw)
 
     @abstractmethod
-    def generate_data(self, rank, world_size):
+    def _generate_data(self, rank, world_size):
         pass
 
     @abstractmethod
-    def plot_data(self, *, data_save, fig_save, **kw):
+    def plot_data(self, **kw):
         pass
 
-    def run(self, rank, world_size):
-        setup(rank, world_size)
-        see_files = [os.path.exists(f) for f in self.output_files]
-        if( all(see_files) ):
-            self.generate_data(rank, world_size)
+    @staticmethod
+    def print_static(*args, level=1, verbose=1, **kw):
+        if verbose >= level:
+            print(*args, **kw, flush=True)
+
+    @staticmethod
+    def plot_inv_record(
+        *,
+        fig_save,
+        init,
+        true,
+        record,
+        name,
+        labels,
+        subplot_args=None,
+        plot_args=None,
+        verbose=1,
+    ):
+        default_subplot = dict(figsize=(10.5, 10.5), sharex=True, sharey=True)
+        default_plot_keys = dict(cmap='gray', aspect='auto')
+
+        if subplot_args is None:
+            subplot_args = {}
+
+        if plot_args is None:
+            plot_args = {}
+
+        subplot_args = {**default_subplot, **subplot_args}
+        plot_args = {**default_plot_keys, **plot_args}
+
+        # Extract the shape of the tensor's dimensions prior to the last 2 dimensions
+        lengths = record.shape[:-2]
+
+        # Reshape the record tensor
+        reshaped_record = record.view(-1, *record.shape[-2:])
+
+        # Create the index map
+        indices = [torch.arange(length) for length in lengths]
+        grid = torch.meshgrid(*indices)
+        index_map = torch.stack(grid, dim=-1).view(-1, len(lengths))
+
+        def subtitle(i):
+            curr = [
+                f'{labels[j][0]}={labels[j][1][index_map[i, j]]}'
+                for j in range(len(lengths))
+            ]
+            return '(' + ', '.join(curr) + ')'
+
+        if plot_args.get('transpose', False):
+            init = init.T
+            true = true.T
+            reshaped_record = reshaped_record.transpose(1, 2)
+            del plot_args['transpose']
+
+        _, ax = plt.subplots(3, **subplot_args)
+        Example.print_static(
+            f'Plotting final results {name} at {fig_save}/final_{name}.jpg...',
+            end='',
+            verbose=verbose,
+        )
+        ax[0].imshow(init, **plot_args)
+        ax[0].set_title(f'{name} Initial')
+        ax[1].imshow(reshaped_record[-1], **plot_args)
+        ax[1].set_title(f'{name} Final')
+        ax[2].imshow(true, **plot_args)
+        ax[2].set_title(f'{name} Ground Truth')
+        ax[1].cla()
+        Example.print_static('SUCCESS', verbose=verbose)
+        for i, curr in enumerate(reshaped_record):
+            save_file_name = f'{fig_save}/{name}_{i}.jpg'
+            Example.print_static(
+                f'Plotting {save_file_name} with params={subtitle(i)}...',
+                end='',
+            )
+            ax[1].imshow(curr, **plot_args)
+            ax[1].set_title(f'{name} {subtitle(i)}')
+            plt.tight_layout()
+            plt.savefig(save_file_name)
+            ax[1].cla()
+            Example.print_static('SUCCESS', verbose=verbose)
+        plt.close()
+        Example.print_static(
+            f'gif creation attempt for {name} at {fig_save}/{name}.gif...',
+            end='',
+        )
+        os.system(
+            'convert -delay 100 -loop 0 '
+            f'$(ls -tr {fig_save}/{name}_*.jpg) '
+            f'{fig_save}/{name}.gif'
+        )
+        Example.print_static('SUCCESS', verbose=verbose)
+        Example.print_static(
+            f'cleaning up {fig_save}/{name}_*.jpg...', end='', verbose=verbose
+        )
+        os.system(f'rm $(ls -t {fig_save}/{name}_*.jpg | tail -n +2)')
+        os.system(
+            f'mv $(ls {fig_save}/{name}_[0-9]*.jpg) {fig_save}/{name}_final.jpg'
+        )
+        Example.print_static('SUCCESS', verbose=verbose)
+
+    def plot_inv_record_auto(
+        self, *, name, labels, subplot_args=None, plot_args=None
+    ):
+        Example.plot_inv_record(
+            fig_save=self.fig_save,
+            init=self.tensors[f'{name}_init'],
+            true=self.tensors[f'{name}_true'],
+            record=self.tensors[f'{name}_record'],
+            name=name,
+            labels=labels,
+            subplot_args=subplot_args,
+            plot_args=plot_args,
+            verbose=self.verbose,
+        )
+
+    def generate_data(self, rank, world_size):
+        self.print(f"Running DDP on rank {rank} / {world_size}.", level=2)
+        self._generate_data(rank, world_size)
+        self.save_all_tensors()
+
+    def plot_field(self, *, field, **kw):
+        raise NotImplementedError(
+            'plot_field not implemented, please override in subclass if you'
+            ' intend to use it'
+        )
+
+    def print(self, *args, level=1, **kw):
+        Example.print_static(*args, level=level, verbose=self.verbose, **kw)
+
+    def save_tensor(self, name):
+        torch.save(self.tensors[name], self.output_files[name])
+
+    def save_all_tensors(self):
+        if set(self.tensors.keys()) != set(self.tensor_names):
+            raise ValueError(
+                'FATAL: tensor_names and self.tensors.keys() do not match.\n'
+                f'    tensor_names: {self.tensor_names}\n'
+                f'    self.tensors.keys(): {self.tensors.keys()}'
+            )
+
+        for name in self.tensors.keys():
+            self.save_tensor(name)
+
+    def load_all_tensors(self):
+        assert all(
+            [
+                a == b
+                for a, b in zip(self.tensor_names, self.output_files.keys())
+            ]
+        )
+        self.tensors = {}
+        paths_exist = [os.path.exists(f) for f in self.output_files.values()]
+        if all(paths_exist):
+            for name, f in self.output_files.items():
+                self.print(f'Load attempt {name} at {f}...', end='')
+                self.tensors[name] = torch.load(f)
+                self.print('SUCCESS')
         else:
-            if( rank == 0 ):
-                print(
+            self.print('FAIL')
+            self.tensors = None
+
+    def plot_fields(self, **kw):
+        for fld, tnsr in self.tensors.items():
+            self.plot_field(field=fld, tensor=tnsr, **kw)
+
+    def run_rank(self, rank, world_size):
+        setup(rank, world_size)
+        self.load_all_tensors()
+        if self.tensors is None:
+            self.tensors = {}
+            self.generate_data(rank, world_size)
+            self.load_all_tensors()
+            if self.tensors is None:
+                raise ValueError(
+                    'FATAL: Data generation failed, check your code'
+                )
+        else:
+            if rank == 0:
+                self.print(
                     'Skipping data generation, delete .pt files in '
-                        f'{data_save} and re-run this script to regenerate', 
-                    flush=True
+                    f'{self.data_save} and re-run this script to '
+                    'regenerate'
                 )
         torch.distributed.barrier()
-    
-    if
-        self.generate_data(rank, world_size)
-        self.plot_data(data_save=self.data_save, fig_save=self.fig_save)
+        if rank == 0:
+            self.plot_data()
+        cleanup()
 
-def MultiscaleExample(Example):
-    def generate_data(self, **kw):
-        pass
-
-    def plot_data(self, **kw):
-        torch.save(vp_record, f'{data_save}/vp_record.pt')
-        torch.save(v_init, f'{data_save}/vp_init.pt')
-        torch.save(v_true, f'{data_save}/vp_true.pt')
- 
-        v = vp_record[-1, -1, :, :]
-        vmin = v_true.min()
-        vmax = v_true.max()
-        cmap = 'gray'
-        _, ax = plt.subplots(3, figsize=(10.5, 10.5), sharex=True,
-                             sharey=True)
-        ax[0].imshow(v.T, aspect='auto', cmap=cmap,
-                     vmin=vmin, vmax=vmax)
-        ax[0].set_title("Initial")
-        ax[1].imshow(v.T, aspect='auto', cmap=cmap,
-                     vmin=vmin, vmax=vmax)
-        ax[1].set_title("Out")
-        ax[2].imshow(v_true.T, aspect='auto', cmap=cmap,
-                     vmin=vmin, vmax=vmax)
-        ax[2].set_title("True")
-        plt.tight_layout()
-        plt.savefig(f'{fig_save}/deepwave_ddp_final_inversion.jpg')
-
-        for freq_idx,epoch in prod(
-            range(vp_record.shape[0]), 
-            range(vp_record.shape[1])
-        ):
-            ax[1].imshow(vp_record[freq_idx,epoch].cpu().T, aspect='auto', cmap=cmap,
-                       vmin=vmin, vmax=vmax)
-            ax[1].set_title(f"Epoch {epoch}, cutoff freq {freqs[freq_idx]}")
-            plt.savefig(f'{fig_save}/deepwave_ddp_{freq_idx}_{epoch}.jpg')
-        
-        dw_iters = f'{fig_save}/deepwave_ddp_[0-9]*_[0-9]*.jpg'
-        cmd = (
-            f'convert -delay 100 -loop 0 '
-                f'$(ls -tr {dw_iters}) {fig_save}/deepwave_ddp.gif'
+    def run(self):
+        world_size = torch.cuda.device_count()
+        if world_size == 0:
+            raise ValueError(
+                '\nFATAL: No GPUs detected, check your system.\n'
+                '    We currently do not support CPU-only training.'
+            )
+        mp.spawn(
+            self.run_rank, args=(world_size,), nprocs=world_size, join=True
         )
-        print('GIF creation attempt')
-        print(f'    cmd="{cmd}"')
-        os.system(cmd)
-        os.system(f'rm {dw_iters}')
+
+
+def define_names(*tensor_names):
+    def decorator(cls):
+        def new_init(self, *, data_save, fig_save, verbose=1, **kw):
+            super(cls, self).__init__(
+                data_save=data_save,
+                fig_save=fig_save,
+                verbose=verbose,
+                tensor_names=list(tensor_names),
+                **kw,
+            )
+
+        cls.__init__ = new_init
+        return cls
+
+    return decorator
