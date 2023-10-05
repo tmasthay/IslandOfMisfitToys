d7f8563585d8295d3dfe2379abd48b1d738da063
tmasthay <tyler@ices.utexas.edu>
2023-10-05 13:49:46 -0500

    CONFIG: More deletions

--- misfit_toys/fwi/__init__.py
+++ misfit_toys/fwi/__init__.py
@@ -1,12 +1,10 @@
 __all__ = [
     "custom_losses",
     "postprocess",
-    "refactor",
     "modules",
     "config"
 ]
 from .custom_losses import *
 from .postprocess import *
-from .refactor import *
 from .modules import *
 from .config import *
