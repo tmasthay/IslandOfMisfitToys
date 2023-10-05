9bb6bd1d0550b1300a9ff02bf5e8a5996778c335
tmasthay <tyler@ices.utexas.edu>
2023-10-05 12:43:12 -0500

    CONFIG: Update __init__ to reflect changes

--- misfit_toys/fwi/__init__.py
+++ misfit_toys/fwi/__init__.py
@@ -1,20 +1,14 @@
 __all__ = [
-    "elastic_class",
     "driver",
     "custom_losses",
-    "elastic_custom",
     "postprocess",
-    "dist_parallel",
     "refactor",
     "modules",
     "config"
 ]
-from .elastic_class import *
 from .driver import *
 from .custom_losses import *
-from .elastic_custom import *
 from .postprocess import *
-from .dist_parallel import *
 from .refactor import *
 from .modules import *
 from .config import *
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='IslandOfMisfitToys',
-    version='0.3.0',
+    version='0.3.1',
     description='A suite of misfit functions to test FWI',  # Provide a short description here
     author='Tyler Masthay and Yiran Shen',
     author_email='tyler@ices.utexas.edu',
