76ff09f972df7e92312e9efdfa704f14a1605e65
tmasthay <tyler@ices.utexas.edu>
2023-10-05 13:38:50 -0500

    CONFIG: Update __init__ again

--- misfit_toys/fwi/__init__.py
+++ misfit_toys/fwi/__init__.py
@@ -1,12 +1,10 @@
 __all__ = [
-    "driver",
     "custom_losses",
     "postprocess",
     "refactor",
     "modules",
     "config"
 ]
-from .driver import *
 from .custom_losses import *
 from .postprocess import *
 from .refactor import *
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='IslandOfMisfitToys',
-    version='0.3.2',
+    version='0.3.3',
     description='A suite of misfit functions to test FWI',  # Provide a short description here
     author='Tyler Masthay and Yiran Shen',
     author_email='tyler@ices.utexas.edu',
