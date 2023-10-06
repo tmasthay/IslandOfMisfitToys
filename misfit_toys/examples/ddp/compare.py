be281cf4b4654147220438862abab45ad1b38d61
tmasthay <tyler@ices.utexas.edu>
2023-10-06 18:36:35 -0500

    FEATURE: Add compare file to compare results from different runs

---
+++ misfit_toys/examples/ddp/compare.py
@@ -0,0 +1,44 @@
+from ddp import ExampleIOMT
+from ddp_deepwave import MultiscaleExample
+from misfit_toys.examples.example import ExampleComparator
+
+
+def main():
+    common_tensors_names = [
+        "vp_true",
+        "vp_init",
+        "vp_record",
+        "freqs",
+        "loss",
+        "src_amp_y",
+        "src_loc_y",
+        "rec_loc_y",
+        "obs_data",
+    ]
+    verbosity = 2
+    iomt_example = ExampleIOMT(
+        data_save="iomt/data",
+        fig_save="iomt/figs",
+        tensor_names=common_tensors_names,
+        verbose=verbosity,
+    )
+    deepwave_example = MultiscaleExample(
+        data_save="deepwave/data",
+        fig_save="deepwave/figs",
+        pickle_save="deepwave/pickle",
+        verbose=verbosity,
+        tensor_names=common_tensors_names,
+    )
+    deepwave_example.n_epochs = 2
+
+    cmp = ExampleComparator(
+        iomt_example,
+        deepwave_example,
+        protect=['freqs'],
+    )
+
+    cmp.compare()
+
+
+if __name__ == "__main__":
+    main()
