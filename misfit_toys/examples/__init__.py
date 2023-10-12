e03801d53e7f739fd11fa12a6f8a0092732df727
tmasthay <tyler@ices.utexas.edu>
2023-10-11 19:11:53 -0500

    FEATURE: Add interactive plotting

--- misfit_toys/examples/__init__.py
+++ misfit_toys/examples/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["download_data", "example", "setup_example", "ddp"]
+__all__ = ["download_data", "example", "setup_example", "jupyter", "ddp"]
 from .download_data import *
 from .example import *
 from .setup_example import *
+from .jupyter import *
 from .ddp import *
