4f21ecb0a3d6edfd3050aa7abb4b9d52df109ec7
tmasthay <tyler@ices.utexas.edu>
2023-10-25 13:31:33 -0500

    REFACTOR: Resurrect download_data in right location

--- misfit_toys/examples/__init__.py
+++ misfit_toys/examples/__init__.py
@@ -1,6 +1,14 @@
-__all__ = ["download_data", "example", "setup_example", "ddp", "jupyter"]
+__all__ = [
+    "download_data",
+    "example",
+    "setup_example",
+    "ddp",
+    "jupyter",
+    "data",
+]
 from .download_data import *
 from .example import *
 from .setup_example import *
 from .ddp import *
 from .jupyter import *
+from .data import *
---
+++ misfit_toys/examples/data/download_data.py
@@ -0,0 +1,27 @@
+from misfit_toys.data.dataset import DataFactory
+from masthay_helpers.global_helpers import iprint
+
+
+def download_data(storage, exclusions):
+    DataFactory.create_database(storage=storage, exclusions=exclusions)
+
+    iprint(
+        (
+            "To see more examples, look at the source code located in"
+            f" {__file__}.\n"
+        ),
+        (
+            "You can still access those examples directly through misfit_toys."
+            "examples if you do not need the actual source code."
+        ),
+    )
+
+
+def main():
+    storage = "conda/data"
+    exclusions = ["das_curtin", "openfwi"]
+    download_data(storage, exclusions)
+
+
+if __name__ == "__main__":
+    main()
