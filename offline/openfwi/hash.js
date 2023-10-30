d3a414d39861891abcbd2c77007ba6a3850747d6
tmasthay <tyler@ices.utexas.edu>
2023-10-30 00:26:06 -0500

    FEATURE: Easier parsing now

--- offline/openfwi/hash.js
+++ offline/openfwi/hash.js
@@ -12,6 +12,26 @@ function extract_hashes(marker) {
     return extractedArray;  // Returns the array of captured groups
 }
 
+function constructUrls(hashArray) {
+    // Use the map() method to create a new array with URLs constructed from each hash
+    let urlArray = hashArray.map(extracted_hash => `https://drive.google.com/file/d/${extracted_hash}/view?usp=drive_link`);
+    return urlArray;  // Return the new array of URLs
+}
+
+function constructUrlDict(basename, urls) {
+    let urlDict = {};  // Create an empty object to hold the key-value pairs
+    urls.forEach((url, index) => {
+        let key = `${basename}${index + 1}`;
+        urlDict[key] = url;
+    });
+    return urlDict;  // Return the constructed object
+}
+
 // Usage
 let marker = 'KCtMme';
-extract_hashes(marker);
+let datatype = 'data';
+// let datatype = 'model'; UNCOMMENT for model data
+let hashes = extract_hashes(marker);
+let urls = constructUrls(hashes);
+let d = constructUrlDict(datatype, urls);
+console.log(d)
