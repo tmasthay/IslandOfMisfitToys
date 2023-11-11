9d82c6df980bf1a5fbf8720a5c018bee0b647fa1
tmasthay <tyler@ices.utexas.edu>
2023-11-10 20:28:19 -0600

    CONFIG: Omit legacy nondependency

--- misfit_toys/examples/__init__.py
+++ misfit_toys/examples/__init__.py
@@ -1,5 +1,4 @@
 __all__ = [
-    "legacy",
     "download_data",
     "example",
     "setup_example",
@@ -8,11 +7,10 @@ __all__ = [
     "jupyter",
     "ddp",
 ]
-from .legacy import *
+from .ddp import *
 from .download_data import *
 from .example import *
+from .jupyter import *
 from .setup_example import *
 from .sitecustomize import *
 from .source import *
-from .jupyter import *
-from .ddp import *
