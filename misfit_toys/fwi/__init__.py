0612a29047b734438da86427eaa700fb8d01cb55
tmasthay <tyler@ices.utexas.edu>
2023-12-07 00:29:35 -0600

    RM: fwi directory

--- misfit_toys/fwi/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["custom_losses", "postprocess", "fwi", "modules"]
-from .custom_losses import *
-from .postprocess import *
-from .fwi import *
-from .modules import *
--- misfit_toys/fwi/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ["distribution", "visual", "seismic_data", "models", "training"]
-from .distribution import *
-from .visual import *
-from .seismic_data import *
-from .models import *
-from .training import *
