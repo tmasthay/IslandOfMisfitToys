fbfccf705e188a36b2cf204f79e853cd0b1087b0
tmasthay <tyler@ices.utexas.edu>
2023-12-07 00:39:41 -0600

    RM: Examples

--- misfit_toys/data/download_data.py
+++ misfit_toys/data/download_data.py
@@ -8,14 +8,10 @@ def download_data(storage, exclusions):
     DataFactory.create_database(storage=storage, exclusions=exclusions)
 
     iprint(
-        (
-            "To see more examples, look at the source code located in"
-            f" {__file__}.\n"
-        ),
-        (
-            "You can still access those examples directly through misfit_toys."
-            "examples if you do not need the actual source code."
-        ),
+        "To see more examples, look at the source code located in"
+        f" {__file__}.\n",
+        "You can still access those examples directly through misfit_toys."
+        "examples if you do not need the actual source code.",
     )
 
 
