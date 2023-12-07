eb4546a4a0b18f58b650a360dc1182cb84a278b5
tmasthay <tyler@ices.utexas.edu>
2023-12-07 01:43:42 -0600

    REFACTOR: Move modules up to fwi level
    ----- SQUASHED AUTO-COMMIT FROM GITHUB ACTIONS -----
    AUTO: Adding .out files generated from pytest. [skip ci]
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch ty/sprint/tccs-fall-purge
    # Your branch is up to date with 'origin/ty/sprint/tccs-fall-purge'.
    #
    # Changes to be committed:
    #	modified:   misfit_toys/examples/marmousi/val/fwi_parallel.py
    #	modified:   misfit_toys/fwi/__init__.py
    #	renamed:    misfit_toys/fwi/modules/models.py -> misfit_toys/fwi/models.py
    #	deleted:    misfit_toys/fwi/modules/__init__.py
    #	renamed:    misfit_toys/fwi/modules/seismic_data.py -> misfit_toys/fwi/seismic_data.py
    #	renamed:    misfit_toys/fwi/modules/training.py -> misfit_toys/fwi/training.py
    #	modified:   tests/benchmarks/out/validate.out
    #
    # Untracked files:
    #	misfit_toys/data/custom/__init__.py
    #	misfit_toys/data/marmousi/deepwave_example/shots16/twolayer/__init__.py
    #	misfit_toys/examples/__init__.py
    #	misfit_toys/examples/marmousi/__init__.py
    #	misfit_toys/examples/marmousi/alan/__init__.py
    #	misfit_toys/examples/marmousi/alan/out/
    #	misfit_toys/examples/marmousi/val/__init__.py
    #	misfit_toys/examples/marmousi/val/out/
    #	misfit_toys/fwi/modules/
    #

--- misfit_toys/fwi/__init__.py
+++ misfit_toys/fwi/__init__.py
@@ -1,3 +1,6 @@
-__all__ = ["custom_losses", "modules"]
+__all__ = ["seismic_data", "custom_losses", "models", "training", "modules"]
+from .seismic_data import *
 from .custom_losses import *
+from .models import *
+from .training import *
 from .modules import *
--- misfit_toys/fwi/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["seismic_data", "models", "training"]
-from .seismic_data import *
-from .models import *
-from .training import *
