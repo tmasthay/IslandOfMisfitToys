a0e571735a76ea26ed41a239305209f7c266c8f4
tmasthay <tyler@ices.utexas.edu>
2023-12-07 06:54:07 -0600

    REFACTOR: Update __init__ file
    ----- SQUASHED AUTO-COMMIT FROM GITHUB ACTIONS -----
    AUTO: Adding .out files generated from pytest. [skip ci]
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch main
    # Your branch is up to date with 'origin/main'.
    #
    # Changes to be committed:
    #	modified:   misfit_toys/fwi/__init__.py
    #
    # Untracked files:
    #	docs/build/
    #	misfit_toys/data/custom/__init__.py
    #	misfit_toys/data/marmousi/deepwave_example/shots16/twolayer/__init__.py
    #	misfit_toys/examples/__init__.py
    #	misfit_toys/examples/marmousi/__init__.py
    #	misfit_toys/examples/marmousi/alan/__init__.py
    #	misfit_toys/examples/marmousi/alan/out/
    #	misfit_toys/examples/marmousi/val/__init__.py
    #	misfit_toys/examples/marmousi/val/out/
    #	source/
    #

--- misfit_toys/fwi/__init__.py
+++ misfit_toys/fwi/__init__.py
@@ -1,6 +1,5 @@
-__all__ = ["seismic_data", "custom_losses", "models", "training", "modules"]
+__all__ = ["seismic_data", "custom_losses", "models", "training"]
 from .seismic_data import *
 from .custom_losses import *
 from .models import *
 from .training import *
-from .modules import *
