15ea117f65ab8507fae1fdcb4b618362c43be8c8
tmasthay <tyler@ices.utexas.edu>
2024-01-10 10:08:42 -0600

    OVERKILL: Don't need this google drive stuff

--- misfit_toys/data/openfwi/factory.py
+++ misfit_toys/data/openfwi/factory.py
@@ -1,89 +1,144 @@
-from subprocess import check_output as co
 import os
 from gdown import download
 import numpy as np
 import torch
-import argparse
 
-from masthay_helpers.global_helpers import add_root_package_path, DotDict
 
-import multiprocessing as mproc
-
-curr_dir = os.path.dirname(__file__)
-add_root_package_path(path=os.path.dirname(__file__), pkg="misfit_toys")
 from misfit_toys.swiffer import sco
-from misfit_toys.data.dataset import DataFactory, towed_src, fixed_rec
-from misfit_toys.utils import parse_path, get_pydict
-from scipy.ndimage import gaussian_filter
-import deepwave as dw
-from time import sleep
+from misfit_toys.data.dataset import DataFactory
+
+# from misfit_toys.utils import get_pydict
+
+import requests
+from googleapiclient.discovery import build
+from tqdm import tqdm
+
+
+def convert_size(num, unit):
+    num = float(num)
+    base = 1024 if unit.endswith('iB') else 1000
+    prefix = unit.replace('iB', 'B').replace('B', '').upper()
+    prot = {
+        '': num,
+        'K': num * base,
+        'M': num * base**2,
+        'G': num * base**3,
+        'T': num * base**4,
+        'P': num * base**5,
+        'E': num * base**6,
+        'Z': num * base**7,
+    }
+    return prot[prefix]
+
+
+def download_public_file(
+    *,
+    file_id,
+    file_name,
+    api_key,
+    dest='',
+    chunk_size=1024,
+    file_size=0.0,
+    static_file_size=False,
+):
+    """
+    Download a public file from Google Drive.
+
+    :param file_id: ID of the file to download.
+    :param file_name: Name of the file (for saving locally).
+    :param api_key: Your Google API key.
+    """
+    base_url = f"https://www.googleapis.com/drive/v3/files/{file_id}"
+    params = {"key": api_key, "alt": "media"}
+    file_path = os.path.join(dest, file_name)
+    os.makedirs(dest, exist_ok=True)
+    print(f'Requesting {base_url}...', end='')
+    response = requests.get(base_url, params=params, stream=True)
+
+    if response.status_code == 200:
+        print('SUCCESS', end='\r')
+        with open(file_name, 'wb') as f:
+            if static_file_size:
+                total_size = file_size
+            else:
+                total_size = int(
+                    response.headers.get('content-length', file_size)
+                )
+
+            with open(file_path, 'wb') as f, tqdm(
+                desc=file_name,
+                total=total_size,
+                unit='B',
+                unit_scale=True,
+                unit_divisor=1000,
+                bar_format=(dest + '/{l_bar}{bar:10}{r_bar}{bar:-10b}'),
+            ) as bar:
+                for chunk in response.iter_content(chunk_size):
+                    size = f.write(chunk)
+                    bar.update(size)
+    else:
+        print(
+            f"FAIL...HTTP status code: {response.status_code}:"
+            f" {response.reason}"
+        )
+
+
+def list_and_download_public_files(
+    *,
+    api_key,
+    folder_id,
+    num_files,
+    dest='',
+    chunk_size=1024,
+    file_size=0.0,
+    static_file_size=False,
+):
+    """
+    List and download files in a public Google Drive folder.
+
+    :param api_key: Your Google API key.
+    :param folder_id: The ID of the public Google Drive folder.
+    :param num_files: Number of files to list in one request.
+    """
+    service = build('drive', 'v3', developerKey=api_key)
+
+    results = (
+        service.files()
+        .list(
+            q=f"'{folder_id}' in parents",
+            pageSize=num_files,
+            fields="nextPageToken, files(id, name)",
+        )
+        .execute()
+    )
+
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        for item in items:
+            download_public_file(
+                file_id=item['id'],
+                file_name=item['name'],
+                dest=dest,
+                api_key=api_key,
+                file_size=file_size,
+                static_file_size=static_file_size,
+                chunk_size=chunk_size,
+            )
 
 
 class Factory(DataFactory):
-    def __extend_init__(self):
-        data_urls = get_pydict(self.src_path, filename="data_urls")
-        model_urls = get_pydict(self.src_path, filename="model_urls")
-        self.metadata['data_urls'] = data_urls
-        self.metadata['model_urls'] = model_urls
+    # def __extend_init__(self):
+    #     # data_urls = get_pydict(self.src_path, filename="data_urls")
+    #     # model_urls = get_pydict(self.src_path, filename="model_urls")
+    #     # self.metadata['data_urls'] = data_urls
+    #     # self.metadata['model_urls'] = model_urls
+    #     input(self.metadata)
 
     def _manufacture_data(self):
+        print(self.src_path)
         self.download_all()
-        # pass
-        # if self.installed(
-        #     "vp_true",
-        #     "vp_init",
-        #     "rho_true",
-        #     "src_loc_y",
-        #     "rec_loc_y",
-        #     "obs_data",
-        # ):
-        #     return
-
-        # self.download_all()
-        # d = DotDict(self.metadata)
-        # self.tensors.vp_true = torch.load(
-        #     os.path.join(self.out_path, 'model1.pt')
-        # )[0].squeeze()
-        # self.tensors.vp = self.tensors.vp_true.to(self.device)
-        # # self.tensors.vp_init = torch.tensor(
-        # #     1 / gaussian_filter(1 / self.tensors.vp_true.cpu().numpy(), 40)
-        # # )
-        # self.tensors.vp_init = self.tensors.vp_true.mean() * torch.ones_like(
-        #     self.tensors.vp_true
-        # )
-        # d.ny, d.nx = self.tensors.vp_true.shape
-        # self.tensors.src_loc_y = towed_src(
-        #     n_shots=d.n_shots,
-        #     src_per_shot=d.src_per_shot,
-        #     d_src=d.d_src,
-        #     fst_src=d.fst_src,
-        #     src_depth=d.src_depth,
-        #     d_intra_shot=d.d_intra_shot,
-        # ).to(self.device)
-        # self.tensors.rec_loc_y = fixed_rec(
-        #     n_shots=d.n_shots,
-        #     rec_per_shot=d.rec_per_shot,
-        #     d_rec=d.d_rec,
-        #     fst_rec=d.fst_rec,
-        #     rec_depth=d.rec_depth,
-        # ).to(self.device)
-        # self.tensors.src_amp_y = (
-        #     dw.wavelets.ricker(d.freq, d.nt, d.dt, d.peak_time)
-        #     .repeat(d.n_shots, d.src_per_shot, 1)
-        #     .to(self.device)
-        # )
-        # print(f"Building obs_data in {self.out_path}...", end="", flush=True)
-        # self.tensors.obs_data = dw.scalar(
-        #     self.tensors.vp,
-        #     d.dy,
-        #     d.dt,
-        #     source_amplitudes=self.tensors.src_amp_y,
-        #     source_locations=self.tensors.src_loc_y,
-        #     receiver_locations=self.tensors.rec_loc_y,
-        #     pml_freq=d.freq,
-        #     accuracy=d.accuracy,
-        # )[-1]
-        # print("SUCCESS", flush=True)
 
     def download_instance(self, k, indices='all'):
         prev_res = [
@@ -139,22 +194,24 @@ class Factory(DataFactory):
     # NOTE: Google Drive limits the number of downloads per day, so users
     #    should only use this if they only want a subset of the data.
     def download_all(self):
-        indices = self.get_download_indices()
-        self.download_instance('data', indices)
-        self.download_instance('model', indices)
-
-    # this is not useful because the network download speed
-    #    gets divided by the number of processes on each subprocess
-    #    e.g, 100 MB/s on 1 process -> 25 MB/s on 4 processes
-    #    TODO: Delete this once you are 100% sure it is not useful
-    def download_all_parallel_notuseful_deprecated(self):
-        indices = self.get_download_indices()
-
-        with mproc.Pool(processes=mproc.cpu_count()) as pool:
-            pool.map(
-                self.download_instance_packed,
-                [('data', indices), ('model', indices)],
-            )
+        common = {
+            'api_key': self.metadata['api_key'],
+            'dest': self.out_path,
+            'static_file_size': self.metadata['static_file_size'],
+            'chunk_size': self.metadata.get('chunk_size', 2048),
+            'static_file_size': self.metadata.get('static_file_size', True),
+            'num_files': self.metadata.get('num_files', 60),
+        }
+        list_and_download_public_files(
+            **common,
+            folder_id=self.metadata['model_folder_id'],
+            file_size=self.metadata.get('model_size', 0.0),
+        )
+        list_and_download_public_files(
+            **common,
+            folder_id=self.metadata['data_folder_id'],
+            file_size=self.metadata['data_size'],
+        )
 
     @staticmethod
     def get_hashes(filename):
--- misfit_toys/data/openfwi/metadata.py
+++ misfit_toys/data/openfwi/metadata.py
@@ -1,11 +1,21 @@
 from masthay_helpers.global_helpers import save_metadata
+import os
 
 
 @save_metadata(cli=True)
 def metadata():
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(file_path, 'api_key.secret'), 'r') as f:
+        api_key = f.read().strip()
+
     return {
         'ext': 'npy',
-        'num_urls': 60,
+        'num_files': 60,
+        'api_key': api_key,
+        'chunk_size': 1024,
+        'data_size': int(667.7 * 1024.0**2),
+        'model_size': int(9.4 * 1024**2),
+        'static_file_size': True,
         'mode': 'front',
         'ny': 70,
         'nx': 70,
@@ -26,7 +36,16 @@ def metadata():
         'freq': 25,
         'peak_time': 1.5 / 25,
         'accuracy': 8,
-        'derived': {'FlatVel_A': {}, 'CurveVel_A': {}},
+        'derived': {
+            'FlatVel_A': {
+                'data_folder_id': '1arNrV9M65cl70ANkBwkg7bi7SI5JtsYQ',
+                'model_folder_id': '1IBM_04bPCBnSMO1TMdaps3EHV5dYhYJa',
+            },
+            'CurveVel_A': {
+                'data_folder_id': '1ry69BNdgG4eTIkKGbnTiYCVhjMKo0THw',
+                'model_folder_id': '1AL7z9TQVgzUdv3yrHB6f_wP2TRvVTKD0',
+            },
+        },
     }
 
 
