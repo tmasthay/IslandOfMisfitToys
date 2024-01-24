75091b1864ff374d13f3709cdebb5e7316d4a7cc
tmasthay <tyler@ices.utexas.edu>
2024-01-24 00:12:14 -0600

    CONFIG: Update __init__ files

--- misfit_toys/fwi/loss/__init__.py
+++ misfit_toys/fwi/loss/__init__.py
@@ -1,7 +1,5 @@
-__all__ = ["w2", "tmp", "a", "tmp2", "tikhonov", "renorm"]
+__all__ = ["w2", "a", "tikhonov", "renorm"]
 from .w2 import *
-from .tmp import *
 from .a import *
-from .tmp2 import *
 from .tikhonov import *
 from .renorm import *
