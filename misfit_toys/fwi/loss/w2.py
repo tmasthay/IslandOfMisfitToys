BUG: Error in gradient?
Our misfit is fine after the small constant adding.

The gradient does not match the expected output.
    The CDF is not STRICTLY increasing though.
    This implies the quantile is a pseudo-inverse, NOT a true inverse.
    So the W2 gradient for this test case may not be the best test.

TODO: Reconsider a different class of PDFs for testing which have
        support over the entire domain.
    Examples may include:
        - Exponential
        - Custom defined PDF that gives you
            (a) a strictly increasing CDF
            (b) an easy quantile and quantile derivative expression
            (c) values in (b) such that Q(CDF(t)) is a simple expression.

misfit_toys/fwi/loss/w2.py
@@ -129,47 +129,8 @@ def true_quantile(
             )
 
         indices = torch.searchsorted(cdf, p)
-        last_low_idx = (cdf <= ltol).nonzero(as_tuple=True)[0]
-        fst_hi_idx = (cdf >= 1 - rtol).nonzero(as_tuple=True)[0]
-
-        # assert (
-        #     len(last_low_idx) > 0
-        # ), f'last_low_idx = {last_low_idx}, min = {cdf.min()}'
-
-        # if len(last_low_idx) == 0:
-        #     msg = f'last_low = {last_low_idx}, min = {cdf.min()}, ltol={ltol}'
-        #     raise ValueError(msg)
-        # if len(fst_hi_idx) == 0:
-        #     msg = f'fst_hi = {fst_hi_idx}, max = {cdf.max()}, rtol={rtol}'
-        #     raise ValueError(msg)
-
-        # last_low_idx = [0]
-        if len(last_low_idx) == 0:
-            last_low_idx = 0
-        else:
-            last_low_idx = last_low_idx[-1] + 1
-        if len(fst_hi_idx) == 0:
-            fst_hi_idx = len(x) - 1
-        else:
-            fst_hi_idx = fst_hi_idx[0] - 1
-
-        # raise ValueError(last_low_idx)
-
-        # input((cdf <= ltol).nonzero(as_tuple=True)[0][-1])
-
-        # last_low_idx = 0
-        # fst_hi_idx = len(x) - 2
-        indices = torch.clamp(
-            indices, last_low_idx, min(fst_hi_idx, len(x) - 1)
-        )
-
-        # integral = torch.floor(indices)
-        # remainder = indices - integral
-        # right_indices = (integral + 1).long()
-        # integral = integral.long()
-        # res = (1 - remainder) * x[integral] + remainder * x[right_indices]
-
-        res = x[indices.long()]
+        indices = torch.clamp(indices, 0, len(x) - 1)
+        res = torch.tensor([x[i] for i in indices])
 
         return res
     else:
@@ -438,31 +399,31 @@ def eval_w2_nonpickable(quantiles):
         if cdf is None:
             cdf = cum_trap(pdf, dx=t[1] - t[0], dim=-1)
         transport_map = quantiles(cdf)
-        dtransport = torch.diff(transport_map, dim=-1)
-        tol = 1e-5
-        dt = t[1] - t[0]
-        start_time = time()
-        for idx, _ in bool_slice(*dtransport.shape, none_dims=[-1]):
-            curr = 0
-            currd = dtransport[idx]
-            while currd[curr] < tol:
-                curr += 1
-            curr_slice = [*idx[:-1], slice(0, curr)]
-            right_val = transport_map[idx][curr]
-            slope = currd[curr : (curr + 100)].mean() / dt
-            interpolant = slope * (t[:curr] - t[curr]) + right_val
-            transport_map[curr_slice] = interpolant
-        for idx, _ in bool_slice(*dtransport.shape, none_dims=[-1]):
-            currd = dtransport[idx]
-            curr = len(currd) - 1
-            while currd[curr] < tol:
-                curr -= 1
-            curr_slice = [*idx[:-1], slice(curr, None)]
-            left_val = transport_map[idx][curr]
-            slope = currd[(curr - 100) : curr].mean() / dt
-            interpolant = slope * (t[curr:] - t[curr]) + left_val
-            transport_map[curr_slice] = interpolant
-        print(f'Elapsed time = {time() - start_time}')
+        # dtransport = torch.diff(transport_map, dim=-1)
+        # tol = 1e-5
+        # dt = t[1] - t[0]
+        # start_time = time()
+        # for idx, _ in bool_slice(*dtransport.shape, none_dims=[-1]):
+        #     curr = 0
+        #     currd = dtransport[idx]
+        #     while currd[curr] < tol:
+        #         curr += 1
+        #     curr_slice = [*idx[:-1], slice(0, curr)]
+        #     right_val = transport_map[idx][curr]
+        #     slope = currd[curr : (curr + 100)].mean() / dt
+        #     interpolant = slope * (t[:curr] - t[curr]) + right_val
+        #     transport_map[curr_slice] = interpolant
+        # for idx, _ in bool_slice(*dtransport.shape, none_dims=[-1]):
+        #     currd = dtransport[idx]
+        #     curr = len(currd) - 1
+        #     while currd[curr] < tol:
+        #         curr -= 1
+        #     curr_slice = [*idx[:-1], slice(curr, None)]
+        #     left_val = transport_map[idx][curr]
+        #     slope = currd[(curr - 100) : curr].mean() / dt
+        #     interpolant = slope * (t[curr:] - t[curr]) + left_val
+        #     transport_map[curr_slice] = interpolant
+        # print(f'Elapsed time = {time() - start_time}')
         diff = t_expand - transport_map
         res = torch.trapezoid(diff**2 * pdf, dx=t[1] - t[0], dim=-1)
         if return_all:

misfit_toys/utils.py
@@ -35,6 +35,7 @@ import torch
 import torch.distributed as dist
 import torch.nn.functional as F
 from masthay_helpers.global_helpers import DotDict, ctab, find_files, vco
+from returns.curry import curry
 from torchaudio.functional import biquad
 
 
@@ -723,7 +724,21 @@ def clean_idx(idx, show_colons=True):
     return f'({", ".join(res)})'
 
 
-def tensor_summary(t, num=5):
+@curry
+def tensor_summary(t, num=5, inc='all', exc=None):
+    num = min(num, t.numel())
+    if inc == 'all':
+        inc = [
+            'shape',
+            'dtype',
+            'min',
+            'max',
+            'mean',
+            'std',
+            f'top {num} values',
+            f'bottom {num} values',
+        ]
+    inc = set(inc).difference(exc or [])
     d = {
         'shape': t.shape,
         'dtype': t.dtype,
@@ -736,6 +751,7 @@ def tensor_summary(t, num=5):
             0
         ],
     }
+    d = {k: v for k, v in d.items() if k in inc}
     s = ''
     for k, v in d.items():
         s += f'{k}:    {v}\n'

tests/unit/fwi/loss/test_w2.py
@@ -24,6 +24,34 @@ from misfit_toys.utils import tensor_summary
 c = DotDict(easy_cfg(os.path.join(os.path.dirname(__file__), 'cfg')))
 
 
+def rerr(s):
+    raise ValueError(s)
+
+
+def rctx(ctx):
+    d = {k: v for k, v in ctx.items() if not k.startswith('_')}
+    raise ValueError(DotDict(d))
+
+
+def rtens(ctx, toi=torch.Tensor, reduce=(lambda x: x)):
+    d = {}
+
+    def helper(k, v):
+        if isinstance(v, toi):
+            return {k: reduce(v)}
+        elif isinstance(v, dict):
+            u = {k: helper(kk, vv) for kk, vv in v.items()}
+            u = {k: v for k, v in u.items() if v}
+            return u
+        else:
+            return {}
+
+    for k, v in ctx.items():
+        d.update(helper(k, v))
+
+    raise ValueError(DotDict(d))
+
+
 def dnumpy(f):
     def helper(x, *args, **kwargs):
         return torch.from_numpy(f(x.numpy(), *args, **kwargs))
@@ -78,9 +106,10 @@ def ref():
     mask = (c.t >= c.shift[:, None]) & (
         c.t <= c.shift[:, None] + c.scale[:, None, None]
     )
-    uniform_pdfs = mask.float() / c.scale[:, None, None]
-    uniform_pdfs[:, :, : c.neps] = c.eps
-    uniform_pdfs[:, :, -(1 + c.neps) :] = c.eps
+    hat_function = c.eps * torch.exp(
+        -((c.t - c.t[len(c.t) // 2]) ** 2) / (2 * c.sig**2)
+    )
+    uniform_pdfs = mask.float() / c.scale[:, None, None] + hat_function
     uniform_pdfs = uniform_pdfs / torch.trapz(
         uniform_pdfs, dx=c.t[1] - c.t[0], dim=-1
     ).unsqueeze(-1)
@@ -128,9 +157,10 @@ def ref():
         )
         v = splev(c.p.numpy(), u)
         w = splev(c.p.numpy(), u, der=1)
+        for _ in range(c.filt.deriv.apps):
+            w = uniform_filter(w, size=c.filt.deriv.size)
         splines[idx] = torch.from_numpy(v)
         splines_deriv[idx] = torch.from_numpy(w)
-
     q = spline_func(c.p, splines.unsqueeze(-1))
     qd = spline_func(c.p, splines_deriv.unsqueeze(-1))
 
@@ -182,19 +212,39 @@ def test_shapes(evaluation):
 
 def test_eval(evaluation):
     d = evaluation
-    shifts, scales = c.shift[:, None], c.scale[None, :]
+    bshifts = c.shift[:, None, None]
+    bscales = c.scale[None, :, None]
+    bt = c.t[None, None, :]
     # raise ValueError(shifts)
     assert abs(c.ref_left) < c.tol
     # assert abs(c.ref_scale - 1.0) < c.tol
-    ds = scales - c.ref_scale
+    ds = bscales - c.ref_scale
+    ref_shift = c.shift[len(c.shift) // 2]
+    dshift = bshifts - ref_shift
     # alpha = ds / c.ref_scale
-    # beta = shifts / c.ref_scale
+    # rerr({**locals(), **globals()}.keys())
+    # beta = bshifts / c.ref_scale
     # gamma = ds + beta
-    exp_output = shifts**2 + shifts * ds + ds**2 / 3.0
+    exp_output = bshifts**2 + bshifts * ds + ds**2 / 3.0
+    exp_output = exp_output[..., 0]
     # exp_output = (ds + shifts) ** 2 * 10.0
     torch.save(exp_output, 'exp_output.pt')
-    # raise ValueError(f'({ds.min(), ds.max()})')
-    exp_grad = None
+    # raise ValueError(f'({ds.min(), ds.max()})')b
+
+    exp_grad = (
+        (bt < ref_shift) * (dshift - bt) ** 2
+        + bscales * (bshifts + ds)
+        + (bt >= ref_shift)
+        * (bt <= ref_shift + c.ref_scale)
+        * (
+            (ds**2 - bscales * ds) * bt**2
+            + (2 * bshifts * ds - bscales * bshifts) * bt
+            + bshifts**2
+            + bscales * bshifts
+            + bscales * ds
+        )
+        + (bt > ref_shift + c.ref_scale) * (dshift - bt) ** 2
+    )
     if c.plt.eval:
         plot_eval(
             d,