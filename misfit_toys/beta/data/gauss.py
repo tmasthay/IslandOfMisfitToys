e6cff0170fd80742aa5aad1f2250a3a1c1533880
tmasthay <tyler@ices.utexas.edu>
2024-03-15 00:58:41 -0500

    FEATURE: Add interpolation for better modularity

--- misfit_toys/beta/data/gauss.py
+++ misfit_toys/beta/data/gauss.py
@@ -5,11 +5,10 @@ from mh.core import DotDict
 from misfit_toys.utils import bool_slice
 from mh.typlotlib import get_frames_bool, save_frames
 import os
-from ..helpers import pdf, cdf, cts_quantile
-from ..plotter import gauss_plotter as plotter
+from misfit_toys.beta.helpers import pdf, cdf, cts_quantile, combos
 
 
-def main4():
+def direct_quantile(plotter):
     x = torch.linspace(-5, 5, 25)
     eps = 0.01
     p = torch.linspace(eps, 1.0 - eps, 100)
@@ -37,16 +36,76 @@ def main4():
 
     PDF = pdf(u, x, renorm=renorm)
     CDF = cdf(PDF, x)
-    # Q = get_quantile_lambda(u, x, p=p, renorm=renorm)
+
     Q = cts_quantile(CDF, x, p=p)
-    # Q = Q.reshape(int(np.prod(Q.shape)))
-    # Qcomp = torch.Tensor(np.array([e.evaluate(p) for e in Q])).squeeze()
-    # Qcomp = Qcomp.view(mu.numel(), sig.numel(), -1)
-    # input(Qcomp.shape)
     Qcomp = Q(p).squeeze().detach()
-    # Qcomp = disc_quantile(CDF, x, p=p)
-    # Qref = [norm.ppf(p, loc=m, scale=s) for m, s in combos(mu, sig, flat=True)]
-    # Qref = np.array(Qref).reshape(*Qcomp.shape)
+
+    d = DotDict(
+        dict(
+            x=x,
+            p=p,
+            raw=PDF,
+            cdf=CDF,
+            Q=Qcomp,
+            Qref=Qref,
+            mu=mu,
+            sig=sig,
+            cdf_ref=cdf_ref,
+            pdf_ref=pdf_ref,
+        )
+    )
+    shape = (3, 1)
+    fig, axes = plt.subplots(*shape, figsize=(10, 10))
+    iter = bool_slice(*Qcomp.shape, none_dims=[-1])
+    frames = get_frames_bool(
+        iter=iter,
+        plotter=plotter,
+        data=d,
+        shape=shape,
+        fig=fig,
+        axes=axes,
+    )
+    save_frames(frames, path='prob4.gif', duration=1000)
+
+    out_file = os.path.join(os.getcwd(), 'prob4.gif')
+    out_file = os.path.abspath(out_file)
+    print(f'\nSee {out_file}\n')
+
+    return d
+
+
+def ref_quantile(plotter):
+    x = torch.linspace(-5, 5, 25)
+    eps = 0.01
+    p = torch.linspace(eps, 1.0 - eps, 100)
+    mu = torch.linspace(-2, 2, 3)
+    sig = torch.linspace(0.5, 1.5, 7)
+    u = torch.exp(
+        -((x[None, None, :] - mu[:, None, None]) ** 2)
+        / (2 * sig[None, :, None] ** 2)
+    )
+
+    pdf_ref = u * (1 / (sig[None, :, None] * np.sqrt(2 * np.pi)))
+    cdf_ref = 0.5 * (
+        1.0
+        + torch.erf(
+            (x[None, None, :] - mu[:, None, None])
+            / (sig[None, :, None] * np.sqrt(2))
+        )
+    )
+    Qref = mu[:, None, None] + sig[None, :, None] * np.sqrt(2) * torch.erfinv(
+        2 * p - 1
+    )
+
+    def renorm(v, y):
+        return v / torch.trapz(v, y, dim=-1).unsqueeze(-1)
+
+    PDF = pdf(u, x, renorm=renorm)
+    CDF = cdf(PDF, x)
+    input(PDF.shape)
+
+    Q = cts_quantile(CDF, x, p=p)
+    Qcomp = Q(p).squeeze().detach()
 
     d = DotDict(
         dict(
--- misfit_toys/beta/w2.py
+++ misfit_toys/beta/w2.py
@@ -1,9 +1,1 @@
-from misfit_toys.beta.data.gauss import main4
 
-
-def main5():
-    d = main4()
-
-
-if __name__ == "__main__":
-    main5()
