330eaf76bf3476cfe3053bb752a32f9ff0fc0f70
tmasthay <tyler@ices.utexas.edu>
2024-03-15 20:25:15 -0500

    CONFIG: FORCE_PUSH = False (SHOULD GET REJECTED)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="IslandOfMisfitToys",
-    version="0.4.1",
+    version="0.4.2",
     description="A suite of misfit functions to test FWI",  # Provide a short description here
     author="Tyler Masthay and Yiran Shen",
     author_email="tyler@ices.utexas.edu",
