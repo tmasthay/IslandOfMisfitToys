c39964e84084b90f99a0ae36761c6c0f3ad03874
tmasthay <tyler@ices.utexas.edu>
2024-03-16 09:55:22 -0500

    CONFIG: Update __init__.py files

---
+++ misfit_toys/beta/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "main",
+    "w2",
+    "helpers",
+    "plotter",
+    "cfg",
+    "data"
+]
+from .main import *
+from .w2 import *
+from .helpers import *
+from .plotter import *
+from .cfg import *
+from .data import *
--- misfit_toys/fwi/loss/__init__.py
+++ misfit_toys/fwi/loss/__init__.py
@@ -1,16 +1,10 @@
 __all__ = [
     "w2",
-    "tmp",
     "a",
     "tikhonov",
-    "renorm",
-    "debug",
-    "conf"
+    "renorm"
 ]
 from .w2 import *
-from .tmp import *
 from .a import *
 from .tikhonov import *
 from .renorm import *
-from .debug import *
-from .conf import *
