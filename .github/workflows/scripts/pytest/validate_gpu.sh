397c97677fe0ef854f0a10225e4542b5a5c40f7f
tmasthay <tyler@ices.utexas.edu>
2024-03-31 22:46:16 -0500

    DEVOPS: Only run unit tests in CI for now

--- .github/workflows/scripts/pytest/validate_gpu.sh
+++ .github/workflows/scripts/pytest/validate_gpu.sh
@@ -10,20 +10,20 @@ cd ~/.sandbox/IslandOfMisfitToys
 eval "$(conda shell.bash hook)"
 conda activate dw_sandbox
 cd tests
-pytest -s
+pytest -m unit
 
 pytest_exit_code=$?
 
-false_failure_modes=$(ls tests/status)
-if [ $(echo $false_failure_modes | wc -w) -gt 0 ]; then
-    for mode in $false_failure_modes; do
-        echo "FAIL: $mode"
-    done
-    exit 1
-fi
+# false_failure_modes=$(ls tests/status)
+# if [ $(echo $false_failure_modes | wc -w) -gt 0 ]; then
+#     for mode in $false_failure_modes; do
+#         echo "FAIL: $mode"
+#     done
+#     exit 1
+# fi
 
-if [ $pytest_exit_code -ne 0 ]; then
-    echo "FAIL: TRUE FAIL pytest"
-    exit $pytest_exit_code
-fi
+# if [ $pytest_exit_code -ne 0 ]; then
+#     echo "FAIL: TRUE FAIL pytest"
+#     exit $pytest_exit_code
+# fi
 exit $pytest_exit_code
