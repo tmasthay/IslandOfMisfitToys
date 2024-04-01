556bcafad9bfd1ae0b748dfc4154feb83945716f
tmasthay <tyler@ices.utexas.edu>
2024-03-31 23:01:43 -0500

    CONFIG: Setup outline for future code in comments
    This will make it such that you can put
         my_header [unit, end_to_end, fast]
         in the commit message and the CI workflow
         will only run those marks.
         If nothing is supplied with the marks,
         then it will run all tests.

--- .github/workflows/scripts/pytest/validate_gpu.sh
+++ .github/workflows/scripts/pytest/validate_gpu.sh
@@ -10,7 +10,22 @@ cd ~/.sandbox/IslandOfMisfitToys
 eval "$(conda shell.bash hook)"
 conda activate dw_sandbox
 cd tests
-pytest -m unit
+
+# last_commit_msg_header=$(git log --format=%s -n 1)
+# if [[ "$last_commit_msg_header" == *'['* && "$last_commit_msg_header" == *']'* ]]; then
+#     # Extract string inside the first pair of brackets
+#     marks_msg=${last_commit_msg_header#*[}
+#     marks_msg=${marks_msg%%]*}
+#     if [[ -n "$marks_msg" ]]; then
+#         # If the extracted string is not empty, replace commas with spaces and prefix with -m
+#         the_marks="-m ${marks_msg//,/ }"
+#     else
+#         the_marks=""
+#     fi
+# else
+#     the_marks=""
+# fi
+pytest -s
 
 pytest_exit_code=$?
 
