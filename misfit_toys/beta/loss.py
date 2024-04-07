BUG: Scaling with softplus
Possible source of the bug is the scaling of the
    softplus renormalization. When we scaling with
    scaling parameter == 10.0, direct comparison
    in L1 with tikhonov regularization works fine.
    However, when we scale with 1.0, the comparison
    fails. If we use Wasserstein, this will cause
    issues downstream in the pipeline.

misfit_toys/beta/loss.py
@@ -365,9 +365,11 @@ def transform_loss(f, *, loss, transform):
     return helper
 
 
-def softplus(*, scale=1.0):
+def softplus(*, scale=1.0, t):
     def helper(f):
-        return torch.log(1 + torch.exp(scale * f)) / scale
+        u = torch.log(1 + torch.exp(scale * f)) / scale
+        u = u / torch.trapz(u, t, dim=-1).unsqueeze(-1)
+        return u
 
     return helper
 