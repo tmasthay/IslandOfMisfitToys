997564f912afdd2d2232f30564c6d27e1d55182f
tmasthay <tyler@ices.utexas.edu>
2024-05-11 08:30:55 -0500

    BUG: Synchronization issue on lambda (maybe temporary but failed twice)

--- misfit_toys/beta/sobolev.py
+++ misfit_toys/beta/sobolev.py
@@ -2,7 +2,7 @@ import torch
 from returns.curry import curry
 
 
-def riel(*, obs_data, t, alpha):
+def riel_legacy(*, obs_data, t, alpha):
     kernel = t ** (alpha - 1)
     fint_obs_data = torch.cumulative_trapezoid(kernel * obs_data, t, dim=-1)
 
@@ -10,5 +10,18 @@ def riel(*, obs_data, t, alpha):
         fint_f = torch.cumulative_trapezoid(kernel * f, t, dim=-1)
         integrand = fint_f - fint_obs_data
         return torch.sum(integrand**2)
+
+    return helper
+
+
+def riel(*, obs_data, t, alpha):
+    # kernel = t ** (alpha - 1)
+    # fint_obs_data = torch.cumulative_trapezoid(kernel * obs_data, t, dim=-1)
+
+    def helper(f):
+        # fint_f = torch.cumulative_trapezoid(kernel * f, t, dim=-1)
+        # integrand = fint_f - fint_obs_data
+        # return torch.sum(integrand**2)
+        return ((obs_data - f) ** 2).sum()
 
     return helper
