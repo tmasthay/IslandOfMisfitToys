88929964440c1eeafb18d1bd7e66719b4516cca2
tmasthay <tyler@ices.utexas.edu>
2024-05-13 18:12:39 -0500

    FIX: Minor changes -- autocomplete works!

--- misfit_toys/examples/hydra/main.py
+++ misfit_toys/examples/hydra/main.py
@@ -2,8 +2,13 @@ import hydra
 from omegaconf import DictConfig
 
 
-@hydra.main(config_path="cfg", config_name="config", version_base=None)
+@hydra.main(config_path="cfg", config_name="cfg", version_base=None)
 def main_dummy(cfg: DictConfig) -> None:
+    print('hello')
     from main_worker import main
 
     main(cfg)
+
+
+if __name__ == "__main__":
+    main_dummy()
