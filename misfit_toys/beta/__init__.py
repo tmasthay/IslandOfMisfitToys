5e49351052b8b0ffe248ba1d77ec4df64c86a014
tmasthay <tyler@ices.utexas.edu>
2024-05-21 16:35:55 -0500

    REFACTOR_CHECKPOINT: Remove unit tests for deprecated w2.py

--- misfit_toys/beta/__init__.py
+++ misfit_toys/beta/__init__.py
@@ -1,11 +1,8 @@
 __all__ = [
     "main",
-    "w2",
     "data",
-    "tmp",
     "loss",
     "opt",
-    "cheb",
     "helpers",
     "steps",
     "sobolev",
@@ -13,16 +10,11 @@ __all__ = [
     "renorm",
     "prob",
     "optimizers",
-    "contained",
     "test_splines",
-    "other",
     "data2",
     "cfg",
-    "data",
 ]
 from .cfg import *
-from .cheb import *
-from .contained import *
 from .data import *
 from .data2 import *
 from .helpers import *
@@ -30,12 +22,9 @@ from .loss import *
 from .main import *
 from .opt import *
 from .optimizers import *
-from .other import *
 from .plotter import *
 from .prob import *
 from .renorm import *
 from .sobolev import *
 from .steps import *
 from .test_splines import *
-from .tmp import *
-from .w2 import *
