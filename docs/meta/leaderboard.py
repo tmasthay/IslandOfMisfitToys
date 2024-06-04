2a72dda88fe39c8a5146b53870d88f1c4a406314
tmasthay <tyler@ices.utexas.edu>
2024-06-04 17:16:21 -0500

    DOCS: Leaderboard

--- docs/meta/leaderboard.py
+++ docs/meta/leaderboard.py
@@ -5,32 +5,16 @@ import hydra
 from omegaconf import DictConfig
 
 
-def make_index_rst(leaderboard_dir, param):
-    index_path = os.path.join(leaderboard_dir, 'index.rst')
-    directories = [
-        d
-        for d in os.listdir(leaderboard_dir)
-        if os.path.isdir(os.path.join(leaderboard_dir, d))
-    ]
-
-    with open(index_path, 'w') as file:
-        file.write("Leaderboard\n===========\n\n")
-        file.write(
-            "Welcome to the Leaderboard. Below are the links to the ranking"
-            " details for each entry.\n\n"
-        )
-
-        for dir in sorted(directories, key=lambda x: int(x)):
-            ranking_path = os.path.join(dir, f'{param}.rst')
-            file.write(f"* `{dir} <{ranking_path}>`_\n")
+def idt_lines(s: str, *, idt_str='    ', idt_lvl=1):
+    istr = idt_str * idt_lvl
+    return istr + ('\n' + istr).join(s.split('\n'))
 
 
 def sco(cmd):
     return co(cmd, shell=True).decode().strip()
 
 
-@hydra.main(config_path="cfg", config_name="cfg", version_base=None)
-def main(c: DictConfig):
+def extract_info(c: DictConfig):
     lines = sco(
         f'find {c.root} -name "{c.param}_compare.yaml" -exec grep -H'
         f' "{c.score}" {{}} \; | awk -F\':\' \'{{print $3,$1}}\' | head -n'
@@ -53,6 +37,23 @@ def main(c: DictConfig):
             cmd = f'find {path} -name "*.{ext}"'
             line['img_files'].extend([e for e in sco(cmd).split('\n') if e])
 
+        def sorter():
+            d = c.rst.img.order.get(c.param, None)
+            if d is None:
+                return lambda v: 0
+            else:
+
+                def helper(v):
+                    base = os.path.basename(v).split('.')[0]
+                    if base in d:
+                        return d.index(base)
+                    else:
+                        return float('inf')
+
+                return helper
+
+        line['img_files'].sort(key=sorter())
+
         # Read git and config files
         with open(os.path.join(path, 'git_info.txt'), 'r') as f:
             git_info = f.read().strip()
@@ -69,52 +70,127 @@ def main(c: DictConfig):
                 line[hydra_file] = f.read().strip()
 
     lines.sort(key=lambda x: float(x['score']), reverse=False)
+    return lines
 
+
+def make_index_rst(leaderboard_dir, param):
+    index_path = os.path.join(leaderboard_dir, 'index.rst')
+    directories = [
+        d
+        for d in os.listdir(leaderboard_dir)
+        if os.path.isdir(os.path.join(leaderboard_dir, d))
+    ]
+
+    with open(index_path, 'w') as file:
+        s = """
+Leaderboard
+===========
+
+.. toctree::
+    :maxdepth: 1
+
+""".lstrip()
+
+        for dir in sorted(directories, key=lambda x: int(x)):
+            s += f"    {dir}/{param}\n"
+        file.write(s)
+
+
+def setup_folders(*, name, size):
     rst_root = 'leaderboard'
     os.system(f'rm -rf {rst_root}')
     os.makedirs(rst_root, exist_ok=False)
-    for rank, line in enumerate(lines):
-        curr_root = os.path.join(rst_root, f"{rank+1}")
+    for i in range(size):
+        curr_root = os.path.join(rst_root, f"{i+1}")
         os.makedirs(curr_root, exist_ok=False)
         os.makedirs(os.path.join(curr_root, 'figs'), exist_ok=False)
-        rst_path = os.path.join(curr_root, f"{c.param}.rst")
-        with open(rst_path, 'w') as rst_file:
-            rst_file.write(f"Score: {line['score']}\n")
-            rst_file.write(f"Run Path: {line['path']}\n")
-            rst_file.write(f"{line['git_info']['short']}\n")
-            rst_file.write("`Full Git Diff <full_git_diff>`_\n")
-            rst_file.write(f"overrides.yaml\n{line['overrides']}\n")
-            rst_file.write("`Config YAML <config>`_\n")
-            rst_file.write("`Hydra YAML <hydra>`_\n")
 
-            # Write images
-            rst_file.write("\nImages\n======\n")
-            for img in line['img_files']:
-                # copy the file to local figs directory
-                os.system(f"cp {img} {curr_root}/figs/")
-                rst_file.write(
-                    f".. image:: figs/{os.path.basename(img)}\n   :align:"
-                    " center\n\n"
-                )
 
-            # write hidden toctree so that sphinx will include dependent rst files
-            rst_file.write("\n.. toctree::\n   :hidden:\n\n")
-            for filename in ['full_git_diff', 'config', 'hydra']:
-                rst_file.write(f"   {filename}\n")
+def write_param_file(*, c, rank, line):
+    curr_root = os.path.join(c.folder_name, f"{rank+1}")
+    rst_path = os.path.join(curr_root, f"{c.param}.rst")
+    with open(rst_path, 'w') as rst_file:
+        # rst_file.write(f"Score: {line['score']}\n")
+        # rst_file.write(f"Run Path: {line['path']}\n")
+        # rst_file.write(f"{line['git_info']['short']}\n")
+        # rst_file.write("`Full Git Diff <full_git_diff>`_\n")
+        # rst_file.write(f"overrides.yaml\n{line['overrides']}\n")
+        # rst_file.write("`Config YAML <config>`_\n")
+        # rst_file.write("`Hydra YAML <hydra>`_\n")
+        # write hidden toctree so that sphinx will include dependent rst files
+        img_str = ''
+        for img in line['img_files']:
+            os.system(f"cp {img} {curr_root}/figs/")
+            img_str += (
+                f".. image:: figs/{os.path.basename(img)}\n   :align:"
+                " center\n\n"
+            )
+        title = f"Rank {rank+1}: {line['score']}"
+        s = f"""
+Rank {rank+1}: {line['score']}
+{'=' * len(title)}
 
-        # Write Git diff in a separate file
-        with open(os.path.join(path, 'full_git_diff.rst'), 'w') as diff_file:
-            diff_file.write(line['git_info']['diff'])
-        for filename in ['config', 'hydra']:
-            with open(
-                os.path.join(curr_root, f'{filename}.rst'), 'w'
-            ) as curr_file:
-                s = '    ' + '\n    '.join(line[filename].split('\n'))
-                rst_heading = '.. code-block:: yaml\n\n'
-                curr_file.write(rst_heading + s)
+.. code-block::
+
+    Score: {line['score']}
+    Run Path: {line['path']}
+    Git Info Summary:
+    {idt_lines(line['git_info']['short'])}
+
+.. toctree::
+    :maxdepth: 1
+
+    overrides
+    config
+    hydra
+    full_git_diff
+
+Images
+
+{img_str}
+""".lstrip()
+        rst_file.write(s)
+
+
+def write_git_diff_file(*, c, rank, line):
+    curr_root = os.path.join(c.folder_name, f"{rank+1}")
+    with open(os.path.join(curr_root, 'full_git_diff.rst'), 'w') as diff_file:
+        title = 'Full Git Diff'
+        diff_file.write(f"{title}\n{'=' * len(title)}\n\n")
+        diff_file.write('.. code-block:: \n\n')
+        diff_file.write(idt_lines(line['git_info']['diff']))
+
+
+def write_yaml_rst_block_file(*, c, rank, line, filename):
+    curr_root = os.path.join(c.folder_name, f"{rank+1}")
+    with open(os.path.join(curr_root, f'{filename}.rst'), 'w') as curr_file:
+        s = idt_lines(line[filename])
+        rst_heading = (
+            f"{filename.capitalize().replace('_', ' ')}\n"
+            f"{'=' * len(filename)}\n\n"
+            ".. code-block:: yaml\n\n"
+        )
+        curr_file.write(rst_heading + s)
+
+
+@hydra.main(config_path="cfg", config_name="cfg", version_base=None)
+def main(c: DictConfig):
+    lines = extract_info(c)
+    setup_folders(name=c.folder_name, size=len(lines))
+
+    for rank, line in enumerate(lines):
+        write_param_file(c=c, rank=rank, line=line)
+        write_git_diff_file(c=c, rank=rank, line=line)
+        for filename in ['config', 'hydra', 'overrides']:
+            write_yaml_rst_block_file(
+                c=c, rank=rank, line=line, filename=filename
+            )
+
     # print(lines)
-    make_index_rst(rst_root, param=c.param)
-    os.system(f'rm -rf {c.rst.dest}/{rst_root}; mv {rst_root} {c.rst.dest}')
+    make_index_rst(c.folder_name, param=c.param)
+    os.system(
+        f'rm -rf {c.rst.dest}/{c.folder_name}; mv {c.folder_name} {c.rst.dest}'
+    )
 
 
 if __name__ == "__main__":
