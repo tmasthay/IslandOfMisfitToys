2f76810a42f5d6c4cda8a527e4f61583726f8cc2
tmasthay <tyler@ices.utexas.edu>
2024-07-16 03:02:04 -0500

    CHECKPOINT: Sigsbee is integrated
    TODO: Ideally we get rid of the dependence on
        uploading the .pt files directly.
        However, the way I am currently getting these is
        by using Madagascar, which we don't want to depend on.

--- misfit_toys/data/sigsbee/factory.py
+++ misfit_toys/data/sigsbee/factory.py
@@ -7,7 +7,7 @@ import deepwave as dw
 import torch
 
 # add_root_package_path(path=os.path.dirname(__file__), pkg="misfit_toys")
-from mh.core import DotDict
+from mh.core import DotDictImmutable as DDI
 from scipy.ndimage import gaussian_filter
 
 from misfit_toys.data.dataset import DataFactory, fixed_rec, towed_src
@@ -15,11 +15,55 @@ from misfit_toys.data.dataset import DataFactory, fixed_rec, towed_src
 
 class Factory(DataFactory):
     def _manufacture_data(self):
-        if self.installed("vp_true", "vp_strat"):
+        if self.installed("vp_true", "src_loc_y", "rec_loc_y", "obs_data"):
             return
 
-        self.tensors.vp_true = torch.load(pj(self.src_path, "vmig2A.pt"))
-        self.tensors.vp_strat = torch.load(pj(self.src_path, "vstr2A.pt"))
+        meta = DDI(self.metadata)
+        self.tensors.vp_true = torch.load(pj(self.src_path, "vstr2A.pt"))
+        self.tensors.vp_init = torch.tensor(
+            1 / gaussian_filter(1 / self.tensors.vp_true.numpy(), 40)
+        )
+        self.tensors.vp = self.tensors.vp_true.to(self.device)
+
+        self.tensors.src_loc_y = towed_src(
+            n_shots=meta.n_shots,
+            src_per_shot=meta.src_per_shot,
+            d_src=meta.d_src,
+            fst_src=meta.fst_src,
+            src_depth=meta.src_depth,
+            d_intra_shot=meta.d_intra_shot,
+        ).to(self.device)
+
+        self.tensors.rec_loc_y = fixed_rec(
+            n_shots=meta.n_shots,
+            rec_per_shot=meta.rec_per_shot,
+            d_rec=meta.d_rec,
+            fst_rec=meta.fst_rec,
+            rec_depth=meta.rec_depth,
+        ).to(self.device)
+
+        self.tensors.src_amp_y = (
+            dw.wavelets.ricker(meta.freq, meta.nt, meta.dt, meta.peak_time)
+            .repeat(meta.n_shots, meta.src_per_shot, 1)
+            .to(self.device)
+        )
+
+        print(
+            f"Building obs_data for SIGSBEE-A in {self.out_path}...",
+            end="",
+            flush=True,
+        )
+        self.tensors.obs_data = dw.scalar(
+            self.tensors.vp,
+            meta.dy,
+            meta.dt,
+            source_amplitudes=self.tensors.src_amp_y,
+            source_locations=self.tensors.src_loc_y,
+            receiver_locations=self.tensors.rec_loc_y,
+            pml_freq=meta.freq,
+            accuracy=meta.accuracy,
+        )[-1]
+        print("SUCCESS", flush=True)
 
 
 def main():
--- misfit_toys/data/sigsbee/metadata.py
+++ misfit_toys/data/sigsbee/metadata.py
@@ -3,7 +3,31 @@ from mh.core_legacy import save_metadata
 
 @save_metadata(cli=True)
 def metadata():
-    return {}
+    return {
+        "url": "",
+        "ext": "pt",
+        "ny": 3201,
+        "nx": 1201,
+        "nt": 751,
+        "dy": 0.00762,  # meters
+        "dx": 0.00762,  # meters
+        "dt": 0.004,  # seconds
+        "n_shots": 50,
+        "src_per_shot": 1,
+        "d_src": 20,
+        "fst_src": 10,
+        "src_depth": 2,
+        "rec_per_shot": 350,
+        "d_rec": 3,
+        "fst_rec": 0,
+        "rec_depth": 2,
+        "d_intra_shot": 0,
+        "freq": 25,
+        "peak_time": 1.5 / 25,
+        "accuracy": 8,
+        "vp_true": {"filename": "vstr2A"},
+        "derived": {},
+    }
 
 
 if __name__ == "__main__":
